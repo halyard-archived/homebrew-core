5f77048254f48d5b74cf5e1aa841dab6f2402d7f
Les Aker <me@lesaker.org>
2017-11-23 16:48:09 -0500

    add libusb

---
+++ Formula/libusb-halyard.rb
@@ -0,0 +1,39 @@
+class LibusbHalyard < Formula
+  desc "Library for USB device access"
+  homepage "http://libusb.info"
+  url "https://github.com/libusb/libusb/releases/download/v1.0.21/libusb-1.0.21.tar.bz2"
+  sha256 "7dce9cce9a81194b7065ee912bcd55eeffebab694ea403ffb91b67db66b1824b"
+
+  head do
+    url "https://github.com/libusb/libusb.git"
+
+    depends_on "autoconf-halyard" => :build
+    depends_on "automake-halyard" => :build
+    depends_on "libtool-halyard" => :build
+  end
+
+  option "without-runtime-logging", "Build without runtime logging functionality"
+  option "with-default-log-level-debug", "Build with default runtime log level of debug (instead of none)"
+
+  conflicts_with "libusb", :because => "libusb-halyard replaces libusb"
+
+  def install
+    args = %W[--disable-dependency-tracking --prefix=#{prefix}]
+    args << "--disable-log" if build.without? "runtime-logging"
+    args << "--enable-debug-log" if build.with? "default-log-level-debug"
+
+    system "./autogen.sh" if build.head?
+    system "./configure", *args
+    system "make", "install"
+    pkgshare.install "examples"
+  end
+
+  test do
+    cp_r (pkgshare/"examples"), testpath
+    cd "examples" do
+      system ENV.cc, "-lusb-1.0", "-L#{lib}", "-I#{include}/libusb-1.0",
+             "listdevs.c", "-o", "test"
+      system "./test"
+    end
+  end
+end
