3102d49a5ba44e3c65bafeac2c77ef06e6d5ac14
Les Aker <me@lesaker.org>
2017-11-23 11:52:57 -0500

    update ncurses

--- Formula/ncurses-halyard.rb
+++ Formula/ncurses-halyard.rb
@@ -1,4 +1,4 @@
-class Ncurses < Formula
+class NcursesHalyard < Formula
   desc "Text-based UI library"
   homepage "https://www.gnu.org/software/ncurses/"
   url "https://ftp.gnu.org/gnu/ncurses/ncurses-6.0.tar.gz"
@@ -6,21 +6,16 @@ class Ncurses < Formula
   sha256 "f551c24b30ce8bfb6e96d9f59b42fbea30fa3a6123384172f9e7284bcf647260"
   revision 4
 
-  bottle do
-    sha256 "b0cc6b16a8f3b669442758113dde0771fe7533b00ed6c7476fbe0517ad16d3ca" => :high_sierra
-    sha256 "178411a38ba6a4affcc8f5c6ce351b2f8ea213b97329013ff2fd6385e63a8a4f" => :sierra
-    sha256 "08026e1a7dc4d53ec8bafcb1b0f40db3e9f029068d300fa8ba9275aa15457bdd" => :el_capitan
-  end
-
   keg_only :provided_by_osx
 
-  depends_on "pkg-config" => :build
+  depends_on "pkg-config-halyard" => :build
+
+  conflicts_with 'ncurses', :because => 'ncurses-halyard replaces ncurses'
 
   # stable rollup patch created by upstream see
   # https://invisible-mirror.net/archives/ncurses/6.0/README
-  resource "ncurses-6.0-20170930-patch.sh" do
+  resource "ncurses-patch.sh" do
     url "https://invisible-mirror.net/archives/ncurses/6.0/ncurses-6.0-20170930-patch.sh.bz2"
-    mirror "https://dl.bintray.com/homebrew/mirror/ncurses-6.0-20170930-patch.sh.bz2"
     sha256 "b179b2acf8838f4ed1c75c47db62109777d36a7e6efc1bd4e52e48cbd1bd4121"
   end
 
@@ -35,8 +30,8 @@ class Ncurses < Formula
     (lib/"pkgconfig").mkpath
 
     # stage and apply patch
-    buildpath.install resource("ncurses-6.0-20170930-patch.sh")
-    system "sh", "ncurses-6.0-20170930-patch.sh"
+    buildpath.install resource("ncurses-patch.sh")
+    system "sh", "ncurses-patch.sh"
 
     system "./configure", "--prefix=#{prefix}",
                           "--enable-pc-files",
