48b3ff4b0cd3d6aac7b40ac14d403f933ccb9e6f
Les Aker <me@lesaker.org>
2017-11-23 13:27:52 -0500

    add libmpc

---
+++ Formula/libmpc-halyard.rb
@@ -0,0 +1,49 @@
+class LibmpcHalyard < Formula
+  desc "C library for the arithmetic of high precision complex numbers"
+  homepage "http://multiprecision.org"
+  url "https://ftp.gnu.org/gnu/mpc/mpc-1.0.3.tar.gz"
+  mirror "http://multiprecision.org/mpc/download/mpc-1.0.3.tar.gz"
+  sha256 "617decc6ea09889fb08ede330917a00b16809b8db88c29c31bfbb49cbf88ecc3"
+  revision 1
+
+  depends_on "gmp-halyard"
+  depends_on "mpfr-halyard"
+
+  conflicts_with "libmpc", :because => "libmpc-halyard replaces libmpc"
+
+  def install
+    args = %W[
+      --prefix=#{prefix}
+      --disable-dependency-tracking
+      --with-gmp=#{Formula["gmp"].opt_prefix}
+      --with-mpfr=#{Formula["mpfr"].opt_prefix}
+    ]
+
+    system "./configure", *args
+    system "make"
+    system "make", "check"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <mpc.h>
+      #include <assert.h>
+      #include <math.h>
+
+      int main() {
+        mpc_t x;
+        mpc_init2 (x, 256);
+        mpc_set_d_d (x, 1., INFINITY, MPC_RNDNN);
+        mpc_tanh (x, x, MPC_RNDNN);
+        assert (mpfr_nan_p (mpc_realref (x)) && mpfr_nan_p (mpc_imagref (x)));
+        mpc_clear (x);
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test.c", "-L#{lib}", "-L#{Formula["mpfr"].opt_lib}",
+                   "-L#{Formula["gmp"].opt_lib}", "-lmpc", "-lmpfr",
+                   "-lgmp", "-o", "test"
+    system "./test"
+  end
+end
