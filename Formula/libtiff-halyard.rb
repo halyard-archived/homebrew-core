5292b0d6a4e9bfb7fbced9ab76ad94e7a05941be
Les Aker <me@lesaker.org>
2017-11-23 16:42:57 -0500

    add libtiff

---
+++ Formula/libtiff-halyard.rb
@@ -0,0 +1,51 @@
+class LibtiffHalyard < Formula
+  desc "TIFF library and utilities"
+  homepage "http://libtiff.maptools.org/"
+  url "http://download.osgeo.org/libtiff/tiff-4.0.9.tar.gz"
+  mirror "https://fossies.org/linux/misc/tiff-4.0.9.tar.gz"
+  sha256 "6e7bdeec2c310734e734d19aae3a71ebe37a4d842e0e23dbb1b8921c0026cfcd"
+  revision 1
+
+  option :cxx11
+  option "with-xz", "Include support for LZMA compression"
+
+  depends_on "jpeg-halyard"
+  depends_on "xz-halyard" => :optional
+
+  def install
+    ENV.cxx11 if build.cxx11?
+
+    args = %W[
+      --disable-dependency-tracking
+      --prefix=#{prefix}
+      --without-x
+      --with-jpeg-include-dir=#{Formula["jpeg"].opt_include}
+      --with-jpeg-lib-dir=#{Formula["jpeg"].opt_lib}
+    ]
+    if build.with? "xz"
+      args << "--with-lzma-include-dir=#{Formula["xz"].opt_include}"
+      args << "--with-lzma-lib-dir=#{Formula["xz"].opt_lib}"
+    else
+      args << "--disable-lzma"
+    end
+    system "./configure", *args
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <tiffio.h>
+
+      int main(int argc, char* argv[])
+      {
+        TIFF *out = TIFFOpen(argv[1], "w");
+        TIFFSetField(out, TIFFTAG_IMAGEWIDTH, (uint32) 10);
+        TIFFClose(out);
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test.c", "-L#{lib}", "-ltiff", "-o", "test"
+    system "./test", "test.tif"
+    assert_match(/ImageWidth.*10/, shell_output("#{bin}/tiffdump test.tif"))
+  end
+end
