c73a0e43fb9286765bf8c973c90c5d1acb41629e
Les Aker <me@lesaker.org>
2017-11-23 17:00:53 -0500

    add libxml2

---
+++ libxml2-halyard.rb
@@ -0,0 +1,57 @@
+class Libxml2Halyard < Formula
+  desc "GNOME XML library"
+  homepage "http://xmlsoft.org"
+  url "http://xmlsoft.org/sources/libxml2-2.9.7.tar.gz"
+  mirror "ftp://xmlsoft.org/libxml2/libxml2-2.9.7.tar.gz"
+  sha256 "f63c5e7d30362ed28b38bfa1ac6313f9a80230720b7fb6c80575eeab3ff5900c"
+
+  head do
+    url "https://git.gnome.org/browse/libxml2.git"
+
+    depends_on "autoconf-halyard" => :build
+    depends_on "automake-halyard" => :build
+    depends_on "libtool-halyard" => :build
+    depends_on "pkg-config-halyard" => :build
+  end
+
+  keg_only :provided_by_macos
+
+  conflicts_with "libxml2", :because => "libxml2-halyard replaces libxml2"
+
+  def install
+    system "autoreconf", "-fiv" if build.head?
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}",
+                          "--without-python",
+                          "--without-lzma"
+    system "make", "install"
+
+    cd "python" do
+      # We need to insert our include dir first
+      inreplace "setup.py", "includes_dir = [", "includes_dir = ['#{include}', '#{MacOS.sdk_path}/usr/include',"
+      system "python", "setup.py", "install", "--prefix=#{prefix}"
+    end
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <libxml/tree.h>
+
+      int main()
+      {
+        xmlDocPtr doc = xmlNewDoc(BAD_CAST "1.0");
+        xmlNodePtr root_node = xmlNewNode(NULL, BAD_CAST "root");
+        xmlDocSetRootElement(doc, root_node);
+        xmlFreeDoc(doc);
+        return 0;
+      }
+    EOS
+    args = shell_output("#{bin}/xml2-config --cflags --libs").split
+    args += %w[test.c -o test]
+    system ENV.cc, *args
+    system "./test"
+
+    ENV.prepend_path "PYTHONPATH", lib/"python2.7/site-packages"
+    system "python", "-c", "import libxml2"
+  end
+end
