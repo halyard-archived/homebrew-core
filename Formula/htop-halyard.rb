fe55dfaeeddd09f2c522ebbe5c2a55e501273b24
Les Aker <me@lesaker.org>
2017-11-23 11:55:10 -0500

    add htop

---
+++ Formula/htop-halyard.rb
@@ -0,0 +1,37 @@
+class HtopHalyard < Formula
+  desc "Improved top (interactive process viewer)"
+  homepage "https://hisham.hm/htop/"
+  url "https://hisham.hm/htop/releases/2.0.2/htop-2.0.2.tar.gz"
+  sha256 "179be9dccb80cee0c5e1a1f58c8f72ce7b2328ede30fb71dcdf336539be2f487"
+
+  head do
+    url "https://github.com/hishamhm/htop.git"
+
+    depends_on "autoconf-halyard" => :build
+    depends_on "automake-halyard" => :build
+    depends_on "libtool-halyard" => :build
+  end
+
+  option "with-ncurses", "Build using homebrew ncurses (enables mouse scroll)"
+
+  depends_on "ncurses-halyard" => :optional
+
+  conflicts_with "htop", :because => "htop-halyard replaces htop"
+
+  def install
+    system "./autogen.sh" if build.head?
+    system "./configure", "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  def caveats; <<~EOS
+    htop requires root privileges to correctly display all running processes,
+    so you will need to run `sudo htop`.
+    You should be certain that you trust any software you grant root privileges.
+    EOS
+  end
+
+  test do
+    pipe_output("#{bin}/htop", "q", 0)
+  end
+end
