21ace87405ed9183860daa7e4cd51b0e868c3eec
Les Aker <me@lesaker.org>
2017-11-23 17:05:51 -0500

    add libyaml

---
+++ libyaml-halyard.rb
@@ -0,0 +1,30 @@
+class LibyamlHalyard < Formula
+  desc "YAML Parser"
+  homepage "https://pyyaml.org/wiki/LibYAML"
+  url "https://pyyaml.org/download/libyaml/yaml-0.1.7.tar.gz"
+  mirror "https://mirrors.kernel.org/debian/pool/main/liby/libyaml/libyaml_0.1.7.orig.tar.gz"
+  sha256 "8088e457264a98ba451a90b8661fcb4f9d6f478f7265d48322a196cec2480729"
+
+  conflicts_with "libyaml", :because => "libyaml-halyard replaces libyaml"
+
+  def install
+    system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <yaml.h>
+
+      int main()
+      {
+        yaml_parser_t parser;
+        yaml_parser_initialize(&parser);
+        yaml_parser_delete(&parser);
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test.c", "-L#{lib}", "-lyaml", "-o", "test"
+    system "./test"
+  end
+end
