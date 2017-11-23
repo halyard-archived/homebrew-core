f8471709dd1d1efcdd7853d59e81d3fd882a7750
Les Aker <me@lesaker.org>
2017-11-23 10:28:48 -0500

    add fontconfig

---
+++ Formula/fontconfig-halyard.rb
@@ -0,0 +1,52 @@
+class FontconfigHalyard < Formula
+  desc "XML-based font configuration API for X Windows"
+  homepage "https://wiki.freedesktop.org/www/Software/fontconfig/"
+  url "https://www.freedesktop.org/software/fontconfig/release/fontconfig-2.12.6.tar.bz2"
+  sha256 "cf0c30807d08f6a28ab46c61b8dbd55c97d2f292cf88f3a07d3384687f31f017"
+
+  head do
+    url "https://anongit.freedesktop.org/git/fontconfig", :using => :git
+
+    depends_on "autoconf-halyard" => :build
+    depends_on "automake-halyard" => :build
+    depends_on "libtool-halyard" => :build
+  end
+
+  depends_on "pkg-config-halyard" => :build
+  depends_on "freetype-halyard"
+
+  conflicts_with 'fontconfig', :because => 'fontconfig-halyard replaces fontconfig'
+
+  def install
+    font_dirs = %w[
+      /System/Library/Fonts
+      /Library/Fonts
+      ~/Library/Fonts
+    ]
+
+    if MacOS.version == :sierra
+      font_dirs << "/System/Library/Assets/com_apple_MobileAsset_Font3"
+    elsif MacOS.version == :high_sierra
+      font_dirs << "/System/Library/Assets/com_apple_MobileAsset_Font4"
+    end
+
+    system "autoreconf", "-iv" if build.head?
+    system "./configure", "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--enable-static",
+                          "--with-add-fonts=#{font_dirs.join(",")}",
+                          "--prefix=#{prefix}",
+                          "--localstatedir=#{var}",
+                          "--sysconfdir=#{etc}"
+    system "make", "install", "RUN_FC_CACHE_TEST=false"
+  end
+
+  def post_install
+    ohai "Regenerating font cache, this may take a while"
+    system "#{bin}/fc-cache", "-frv"
+  end
+
+  test do
+    system "#{bin}/fc-list"
+  end
+end
