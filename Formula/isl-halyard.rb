4b39324019285eadd1c63bd234b9e2cb917b165a
Les Aker <me@lesaker.org>
2017-11-23 11:58:56 -0500

    add isl

---
+++ Formula/isl-halyard.rb
@@ -0,0 +1,37 @@
+class IslHalyard < Formula
+  desc "Integer Set Library for the polyhedral model"
+  homepage "http://isl.gforge.inria.fr"
+  url "http://isl.gforge.inria.fr/isl-0.18.tar.xz"
+  mirror "https://mirrors.ocf.berkeley.edu/debian/pool/main/i/isl/isl_0.18.orig.tar.xz"
+  sha256 "0f35051cc030b87c673ac1f187de40e386a1482a0cfdf2c552dd6031b307ddc4"
+
+  depends_on "gmp-halyard"
+
+  conflicts_with "isl", :because => "isl-halyard replaces isl"
+
+  def install
+    system "./configure", "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}",
+                          "--with-gmp=system",
+                          "--with-gmp-prefix=#{Formula["gmp"].opt_prefix}"
+    system "make", "check"
+    system "make", "install"
+    (share/"gdb/auto-load").install Dir["#{lib}/*-gdb.py"]
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <isl/ctx.h>
+
+      int main()
+      {
+        isl_ctx* ctx = isl_ctx_alloc();
+        isl_ctx_free(ctx);
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test.c", "-L#{lib}", "-lisl", "-o", "test"
+    system "./test"
+  end
+end
