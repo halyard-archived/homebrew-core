3d76fff6c7c5b230ca0e6c3f2c487cc260711252
Les Aker <me@lesaker.org>
2017-11-22 23:04:10 -0500

    add bash-completion

---
+++ Formula/bash-completion-halyard.rb
@@ -0,0 +1,29 @@
+class BashCompletionHalyard < Formula
+  desc "Programmable completion for Bash 4.1+"
+  homepage "https://github.com/scop/bash-completion"
+  url "https://github.com/scop/bash-completion/releases/download/2.7/bash-completion-2.7.tar.xz"
+  sha256 "41ba892d3f427d4a686de32673f35401bc947a7801f684127120cdb13641441e"
+  head "https://github.com/scop/bash-completion.git"
+
+  depends_on "bash-halyard"
+
+  def install
+    inreplace "bash_completion", "readlink -f", "readlink"
+
+    system "./configure", "--prefix=#{prefix}", "--sysconfdir=#{etc}"
+    ENV.deparallelize
+    system "make", "install"
+  end
+
+  def caveats; <<~EOS
+    Add the following to your ~/.bash_profile:
+      if [ -f #{HOMEBREW_PREFIX}/share/bash-completion/bash_completion ]; then
+        . #{HOMEBREW_PREFIX}/share/bash-completion/bash_completion
+      fi
+    EOS
+  end
+
+  test do
+    system "test", "-f", "#{share}/bash-completion/bash_completion"
+  end
+end
