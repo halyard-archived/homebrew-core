6b55410f90b30446ad8e783edaeb007762dab2f7
Les Aker <me@lesaker.org>
2017-11-23 12:23:46 -0500

    add ledger

---
+++ Formula/ledger-halyard.rb
@@ -0,0 +1,63 @@
+class LedgerHalyard < Formula
+  desc "Command-line, double-entry accounting tool"
+  homepage "http://ledger-cli.org"
+  url "https://github.com/ledger/ledger/archive/v3.1.1.tar.gz"
+  sha256 "90f06561ab692b192d46d67bc106158da9c6c6813cc3848b503243a9dfd8548a"
+  revision 7
+  head "https://github.com/ledger/ledger.git"
+
+  option "with-debug", "Build with debugging symbols enabled"
+  option "with-docs", "Build HTML documentation"
+  option "without-python", "Build without python support"
+
+  depends_on "cmake-halyard" => :build
+  depends_on "gmp-halyard"
+  depends_on "mpfr-halyard"
+
+  boost_opts = []
+  boost_opts << "c++11" if MacOS.version < "10.9"
+  depends_on "boost-halyard" => boost_opts
+  depends_on "boost-python-halyard" => boost_opts if build.with? "python"
+
+  needs :cxx11
+
+  conflicts_with "ledger", :because => "ledger-halyard replaces ledger"
+
+  def install
+    ENV.cxx11
+
+    flavor = build.with?("debug") ? "debug" : "opt"
+
+    args = %W[
+      --jobs=#{ENV.make_jobs}
+      --output=build
+      --prefix=#{prefix}
+      --boost=#{Formula["boost"].opt_prefix}
+    ]
+    args << "--python" if build.with? "python"
+    args += %w[-- -DBUILD_DOCS=1]
+    args << "-DBUILD_WEB_DOCS=1" if build.with? "docs"
+    system "./acprep", flavor, "make", *args
+    system "./acprep", flavor, "make", "doc", *args
+    system "./acprep", flavor, "make", "install", *args
+
+    (pkgshare/"examples").install Dir["test/input/*.dat"]
+    pkgshare.install "contrib"
+    pkgshare.install "python/demo.py" if build.with? "python"
+    elisp.install Dir["lisp/*.el", "lisp/*.elc"]
+    bash_completion.install pkgshare/"contrib/ledger-completion.bash"
+  end
+
+  test do
+    balance = testpath/"output"
+    system bin/"ledger",
+      "--args-only",
+      "--file", "#{pkgshare}/examples/sample.dat",
+      "--output", balance,
+      "balance", "--collapse", "equity"
+    assert_equal "          $-2,500.00  Equity", balance.read.chomp
+    assert_equal 0, $CHILD_STATUS.exitstatus
+
+    system "python", pkgshare/"demo.py" if build.with? "python"
+  end
+end
