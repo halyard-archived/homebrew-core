85efbee4039ced0d5bc65c9c14b387a6992aa1d8
Les Aker <me@lesaker.org>
2017-11-23 21:42:01 -0500

    add unibilium

---
+++ Formula/unibilium-halyard.rb
@@ -0,0 +1,30 @@
+class UnibiliumHalyard < Formula
+  desc "Very basic terminfo library"
+  homepage "https://github.com/mauke/unibilium"
+  url "https://github.com/mauke/unibilium/archive/v1.2.1.tar.gz"
+  sha256 "6045b4f6adca7b1123284007675ca71f718f70942d3a93d8b9fa5bd442006ec1"
+
+  depends_on "libtool-halyard" => :build
+
+  def install
+    system "make"
+    system "make", "install", "PREFIX=#{prefix}"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <unibilium.h>
+      #include <stdio.h>
+
+      int main()
+      {
+        setvbuf(stdout, NULL, _IOLBF, 0);
+        unibi_term *ut = unibi_dummy();
+        unibi_destroy(ut);
+        return 0;
+      }
+    EOS
+    system ENV.cc, "-I#{include}", "test.c", "-L#{lib}", "-lunibilium", "-o", "test"
+    system "./test"
+  end
+end
