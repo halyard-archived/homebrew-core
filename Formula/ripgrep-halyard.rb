c679f86c06d3909092ab3baad537768a7452adcf
Les Aker <me@lesaker.org>
2017-11-23 19:38:18 -0500

    add ripgrep

---
+++ Formula/ripgrep-halyard.rb
@@ -0,0 +1,30 @@
+class RipgrepHalyard < Formula
+  desc "Search tool like grep and The Silver Searcher"
+  homepage "https://github.com/BurntSushi/ripgrep"
+  url "https://github.com/BurntSushi/ripgrep/archive/0.7.1.tar.gz"
+  sha256 "e010693637acebb409f3dba7caf59ef093d1894a33b14015041b8d43547665f5"
+  head "https://github.com/BurntSushi/ripgrep.git"
+
+  depends_on "rust-halyard" => :build
+
+  conflicts_with "ripgrep", :because => "ripgrep-halyard replaces ripgrep"
+
+  def install
+    system "cargo", "build", "--release"
+
+    bin.install "target/release/rg"
+    man1.install "doc/rg.1"
+
+    # Completion scripts are generated in the crate's build directory, which
+    # includes a fingerprint hash. Try to locate it first
+    out_dir = Dir["target/release/build/ripgrep-*/out"].first
+    bash_completion.install "#{out_dir}/rg.bash-completion"
+    fish_completion.install "#{out_dir}/rg.fish"
+    zsh_completion.install "complete/_rg"
+  end
+
+  test do
+    (testpath/"Hello.txt").write("Hello World!")
+    system "#{bin}/rg", "Hello World!", testpath
+  end
+end
