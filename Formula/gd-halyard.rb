e0f49073114bf4906f19addcf9411d92ea22e66b
Les Aker <me@lesaker.org>
2017-11-23 21:09:30 -0500

    add gd

---
+++ Formula/gd-halyard.rb
@@ -0,0 +1,39 @@
+class GdHalyard < Formula
+  desc "Graphics library to dynamically manipulate images"
+  homepage "https://libgd.github.io/"
+  url "https://github.com/libgd/libgd/releases/download/gd-2.2.5/libgd-2.2.5.tar.xz"
+  sha256 "8c302ccbf467faec732f0741a859eef4ecae22fea2d2ab87467be940842bde51"
+
+  head do
+    url "https://github.com/libgd/libgd.git"
+
+    depends_on "autoconf-halyard" => :build
+    depends_on "automake-halyard" => :build
+    depends_on "libtool-halyard" => :build
+  end
+
+  depends_on "fontconfig-halyard"
+  depends_on "freetype-halyard"
+  depends_on "jpeg-halyard"
+  depends_on "libpng-halyard"
+  depends_on "libtiff-halyard"
+  depends_on "webp-halyard"
+
+  conflicts_with "gd", :because => "gd-halyard replaces gd"
+
+  def install
+    system "./bootstrap.sh" if build.head?
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}",
+                          "--with-freetype=#{Formula["freetype"].opt_prefix}",
+                          "--with-png=#{Formula["libpng"].opt_prefix}",
+                          "--without-x",
+                          "--without-xpm"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/pngtogd", test_fixtures("test.png"), "gd_test.gd"
+    system "#{bin}/gdtopng", "gd_test.gd", "gd_test.png"
+  end
+end
