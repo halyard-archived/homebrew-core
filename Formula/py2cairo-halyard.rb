b4e60e32267deea6770f62919438f17c2eeefb06
Les Aker <me@lesaker.org>
2017-11-24 18:24:33 -0500

    add py2cairo

---
+++ Formula/py2cairo-halyard.rb
@@ -0,0 +1,19 @@
+class Py2cairoHalyard < Formula
+  desc "Python 2 bindings for the Cairo graphics library"
+  homepage "https://cairographics.org/pycairo/"
+  url "https://github.com/pygobject/pycairo/releases/download/v1.15.4/pycairo-1.15.4.tar.gz"
+  sha256 "ee4c3068c048230e5ce74bb8994a024711129bde1af1d76e3276c7acd81c4357"
+
+  depends_on "pkg-config-halyard" => :build
+  depends_on "cairo-halyard"
+
+  conflicts_with "py2cairo", :because => "py2cairo-halyard replaces py2cairo"
+
+  def install
+    system "python", *Language::Python.setup_install_args(prefix)
+  end
+
+  test do
+    system "python", "-c", "import cairo; print(cairo.version)"
+  end
+end
