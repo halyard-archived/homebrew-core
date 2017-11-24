0acc059118610d20991f756810246f04598ce3a2
Les Aker <me@lesaker.org>
2017-11-24 18:10:37 -0500

    add rust

---
+++ Formula/rust-halyard.rb
@@ -0,0 +1,129 @@
+class RustHalyard < Formula
+  desc "Safe, concurrent, practical language"
+  homepage "https://www.rust-lang.org/"
+
+  stable do
+    url "https://static.rust-lang.org/dist/rustc-1.22.1-src.tar.gz"
+    sha256 "8b7a42bdd6eb205a8c533eb41b5c42389a88158d060aed1e0f461f68c1fd3fd3"
+
+    resource "cargo" do
+      url "https://github.com/rust-lang/cargo.git",
+          :tag => "0.22.0",
+          :revision => "3423351a5d75ac7377bb15987842aadcfd068ad2"
+    end
+
+    resource "racer" do
+      url "https://github.com/racer-rust/racer/archive/2.0.12.tar.gz"
+      sha256 "1fa063d90030c200d74efb25b8501bb9a5add7c2e25cbd4976adf7a73bf715cc"
+    end
+  end
+
+  head do
+    url "https://github.com/rust-lang/rust.git"
+
+    resource "cargo" do
+      url "https://github.com/rust-lang/cargo.git"
+    end
+  end
+
+  option "with-llvm", "Build with brewed LLVM. By default, Rust's LLVM will be used."
+  option "with-racer", "Build Racer code completion tool, and retain Rust sources."
+
+  depends_on "cmake-halyard" => :build
+  depends_on "pkg-config-halyard" => :run
+  depends_on "llvm-halyard" => :optional
+  depends_on "openssl-halyard"
+  depends_on "libssh2-halyard"
+
+  conflicts_with "cargo-completion", :because => "both install shell completion for cargo"
+  conflicts_with "rust", :because => "rust-halyard replaces rust"
+
+  # According to the official readme, GCC 4.7+ is required
+  fails_with :gcc_4_0
+  fails_with :gcc
+  ("4.3".."4.6").each do |n|
+    fails_with :gcc => n
+  end
+
+  resource "cargobootstrap" do
+    # From https://github.com/rust-lang/rust/blob/#{version}/src/stage0.txt
+    url "https://static.rust-lang.org/dist/2017-10-12/cargo-0.22.0-x86_64-apple-darwin.tar.gz"
+    sha256 "8968697ae6eef3b178438a6d5563f531e499308dcea7f4915665dbcec54c851a"
+  end
+
+  def install
+    # Fix build failure for compiler_builtins "error: invalid deployment target
+    # for -stdlib=libc++ (requires OS X 10.7 or later)"
+    ENV["MACOSX_DEPLOYMENT_TARGET"] = MacOS.version
+
+    # Fix build failure for cmake v0.1.24 "error: internal compiler error:
+    # src/librustc/ty/subst.rs:127: impossible case reached" on 10.11, and for
+    # libgit2-sys-0.6.12 "fatal error: 'os/availability.h' file not found
+    # #include <os/availability.h>" on 10.11 and "SecTrust.h:170:67: error:
+    # expected ';' after top level declarator" among other errors on 10.12
+    ENV["SDKROOT"] = MacOS.sdk_path
+
+    args = ["--prefix=#{prefix}"]
+    args << "--disable-rpath" if build.head?
+    args << "--llvm-root=#{Formula["llvm-halyard"].opt_prefix}" if build.with? "llvm"
+    if build.head?
+      args << "--release-channel=nightly"
+    else
+      args << "--release-channel=stable"
+    end
+    system "./configure", *args
+    system "make"
+    system "make", "install"
+
+    resource("cargobootstrap").stage do
+      system "./install.sh", "--prefix=#{buildpath}/cargobootstrap"
+    end
+    ENV.prepend_path "PATH", buildpath/"cargobootstrap/bin"
+
+    resource("cargo").stage do
+      ENV["RUSTC"] = bin/"rustc"
+      system "cargo", "build", "--release", "--verbose"
+      bin.install "target/release/cargo"
+    end
+
+    if build.with? "racer"
+      resource("racer").stage do
+        ENV.prepend_path "PATH", bin
+        cargo_home = buildpath/"cargo_home"
+        cargo_home.mkpath
+        ENV["CARGO_HOME"] = cargo_home
+        system bin/"cargo", "build", "--release", "--verbose"
+        (libexec/"bin").install "target/release/racer"
+        (bin/"racer").write_env_script(libexec/"bin/racer", :RUST_SRC_PATH => pkgshare/"rust_src")
+      end
+      # Remove any binary files; as Homebrew will run ranlib on them and barf.
+      rm_rf Dir["src/{llvm,test,librustdoc,etc/snapshot.pyc}"]
+      (pkgshare/"rust_src").install Dir["src/*"]
+    end
+
+    rm_rf prefix/"lib/rustlib/uninstall.sh"
+    rm_rf prefix/"lib/rustlib/install.log"
+  end
+
+  def post_install
+    Dir["#{lib}/rustlib/**/*.dylib"].each do |dylib|
+      chmod 0664, dylib
+      MachO::Tools.change_dylib_id(dylib, "@rpath/#{File.basename(dylib)}")
+      chmod 0444, dylib
+    end
+  end
+
+  test do
+    system "#{bin}/rustdoc", "-h"
+    (testpath/"hello.rs").write <<~EOS
+      fn main() {
+        println!("Hello World!");
+      }
+    EOS
+    system "#{bin}/rustc", "hello.rs"
+    assert_equal "Hello World!\n", `./hello`
+    system "#{bin}/cargo", "new", "hello_world", "--bin"
+    assert_equal "Hello, world!",
+                 (testpath/"hello_world").cd { `#{bin}/cargo run`.split("\n").last }
+  end
+end
