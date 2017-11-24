1647b3ae35f3764463294add98ea71fbd4115c28
Les Aker <me@lesaker.org>
2017-11-23 21:42:44 -0500

    add unibilium conflict

--- Formula/unibilium-halyard.rb
+++ Formula/unibilium-halyard.rb
@@ -6,6 +6,8 @@ class UnibiliumHalyard < Formula
 
   depends_on "libtool-halyard" => :build
 
+  conflicts_with "unibilium", :because => "unibilium-halyard replaces unibilium"
+
   def install
     system "make"
     system "make", "install", "PREFIX=#{prefix}"
