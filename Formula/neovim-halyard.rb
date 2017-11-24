7e3ced954f257ca6155e5f91a3a6749f016c18cb
Les Aker <me@lesaker.org>
2017-11-24 10:06:27 -0500

    add neovim

---
+++ Formula/neovim-halyard.rb
@@ -0,0 +1,59 @@
+class NeovimHalyard < Formula
+  desc "Ambitious Vim-fork focused on extensibility and agility"
+  homepage "https://neovim.io/"
+  url "https://github.com/neovim/neovim/archive/v0.2.2.tar.gz"
+  sha256 "a838ee07cc9a2ef8ade1b31a2a4f2d5e9339e244ade68e64556c1f4b40ccc5ed"
+  head "https://github.com/neovim/neovim.git"
+
+  depends_on "cmake-halyard" => :build
+  depends_on "lua-halyard" => :build
+  depends_on "pkg-config-halyard" => :build
+  depends_on "gettext-halyard"
+  depends_on "jemalloc-halyard"
+  depends_on "libtermkey-halyard"
+  depends_on "libuv-halyard"
+  depends_on "libvterm-halyard"
+  depends_on "luajit-halyard"
+  depends_on "msgpack-halyard"
+  depends_on "unibilium-halyard"
+
+  conflicts_with "neovim", :because => "neovim-halyard replaces neovim"
+
+  resource "lpeg" do
+    url "https://luarocks.org/manifests/gvvaughan/lpeg-1.0.1-1.src.rock", :using => :nounzip
+    sha256 "149be31e0155c4694f77ea7264d9b398dd134eca0d00ff03358d91a6cfb2ea9d"
+  end
+
+  resource "mpack" do
+    url "https://luarocks.org/manifests/tarruda/mpack-1.0.6-0.src.rock", :using => :nounzip
+    sha256 "9068d9d3f407c72a7ea18bc270b0fa90aad60a2f3099fa23d5902dd71ea4cd5f"
+  end
+
+  def install
+    resources.each do |r|
+      r.stage(buildpath/"deps-build/build/src/#{r.name}")
+    end
+
+    ENV.prepend_path "LUA_PATH", "#{buildpath}/deps-build/share/lua/5.1/?.lua"
+    ENV.prepend_path "LUA_CPATH", "#{buildpath}/deps-build/lib/lua/5.1/?.so"
+
+    cd "deps-build" do
+      system "luarocks-5.1", "build", "build/src/lpeg/lpeg-1.0.1-1.src.rock", "--tree=."
+      system "luarocks-5.1", "build", "build/src/mpack/mpack-1.0.6-0.src.rock", "--tree=."
+      system "cmake", "../third-party", "-DUSE_BUNDLED=OFF", *std_cmake_args
+      system "make"
+    end
+
+    mkdir "build" do
+      system "cmake", "..", *std_cmake_args
+      system "make", "install"
+    end
+  end
+
+  test do
+    (testpath/"test.txt").write("Hello World from Vim!!")
+    system bin/"nvim", "--headless", "-i", "NONE", "-u", "NONE",
+                       "+s/Vim/Neovim/g", "+wq", "test.txt"
+    assert_equal "Hello World from Neovim!!", (testpath/"test.txt").read.chomp
+  end
+end
