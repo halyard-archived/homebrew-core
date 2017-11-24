693e712a0935d63801b4978bb2a22d71c1582afb
Les Aker <me@lesaker.org>
2017-11-24 11:12:32 -0500

    add gobject-introspection

---
+++ Formula/gobject-introspection-halyard.rb
@@ -0,0 +1,37 @@
+class GobjectIntrospectionHalyard < Formula
+  desc "Generate introspection data for GObject libraries"
+  homepage "https://live.gnome.org/GObjectIntrospection"
+  url "https://download.gnome.org/sources/gobject-introspection/1.54/gobject-introspection-1.54.1.tar.xz"
+  sha256 "b88ded5e5f064ab58a93aadecd6d58db2ec9d970648534c63807d4f9a7bb877e"
+
+  depends_on "pkg-config-halyard" => :run
+  depends_on "glib-halyard"
+  depends_on "cairo-halyard"
+  depends_on "libffi-halyard"
+
+  conflicts_with "gobject-introspection", :because => "gobject-introspection-halyard replaces gobject-introspection"
+
+  resource "tutorial" do
+    url "https://gist.github.com/7a0023656ccfe309337a.git",
+        :revision => "499ac89f8a9ad17d250e907f74912159ea216416"
+  end
+
+  def install
+    ENV["GI_SCANNER_DISABLE_CACHE"] = "true"
+    inreplace "giscanner/transformer.py", "/usr/share", "#{HOMEBREW_PREFIX}/share"
+    inreplace "configure" do |s|
+      s.change_make_var! "GOBJECT_INTROSPECTION_LIBDIR", "#{HOMEBREW_PREFIX}/lib"
+    end
+
+    system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}", "PYTHON=python"
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    ENV.prepend_path "PKG_CONFIG_PATH", Formula["libffi-halyard"].opt_lib/"pkgconfig"
+    resource("tutorial").stage testpath
+    system "make"
+    assert_predicate testpath/"Tut-0.1.typelib", :exist?
+  end
+end
