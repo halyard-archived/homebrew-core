36d1b925e7d20b0a53c6a13d69bc494e3c0bc787
Les Aker <me@lesaker.org>
2017-11-24 10:58:11 -0500

    add c-ares

---
+++ Formula/c-ares-halyard.rb
@@ -0,0 +1,41 @@
+class CAresHalyard < Formula
+  desc "Asynchronous DNS library"
+  homepage "https://c-ares.haxx.se/"
+  url "https://c-ares.haxx.se/download/c-ares-1.13.0.tar.gz"
+  mirror "https://launchpad.net/ubuntu/+archive/primary/+files/c-ares_1.13.0.orig.tar.gz"
+  sha256 "03f708f1b14a26ab26c38abd51137640cb444d3ec72380b21b20f1a8d2861da7"
+
+  conflicts_with "c-ares", :because => "c-ares-halyard replaces c-ares"
+
+  head do
+    url "https://github.com/bagder/c-ares.git"
+
+    depends_on "automake-halyard" => :build
+    depends_on "autoconf-halyard" => :build
+    depends_on "libtool-halyard" => :build
+  end
+
+  def install
+    system "./buildconf" if build.head?
+    system "./configure", "--prefix=#{prefix}",
+                          "--disable-dependency-tracking",
+                          "--disable-debug"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <stdio.h>
+      #include <ares.h>
+
+      int main()
+      {
+        ares_library_init(ARES_LIB_INIT_ALL);
+        ares_library_cleanup();
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test.c", "-L#{lib}", "-lcares", "-o", "test"
+    system "./test"
+  end
+end
