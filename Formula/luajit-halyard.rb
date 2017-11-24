be13eab997e3968f72237d8deb677ffc31ecdadc
Les Aker <me@lesaker.org>
2017-11-24 10:11:10 -0500

    add luajit

---
+++ Formula/luajit-halyard.rb
@@ -0,0 +1,69 @@
+class LuajitHalyard < Formula
+  desc "Just-In-Time Compiler (JIT) for the Lua programming language"
+  homepage "https://luajit.org/luajit.html"
+  url "https://luajit.org/download/LuaJIT-2.0.5.tar.gz"
+  sha256 "874b1f8297c697821f561f9b73b57ffd419ed8f4278c82e05b48806d30c1e979"
+
+  option "with-debug", "Build with debugging symbols"
+  option "with-52compat", "Build with additional Lua 5.2 compatibility"
+
+  conflicts_with "luajit", :because => "luajit-halyard replaces luajit"
+
+  def install
+    # 1 - Override the hardcoded gcc.
+    # 2 - Remove the "-march=i686" so we can set the march in cflags.
+    # Both changes should persist and were discussed upstream.
+    inreplace "src/Makefile" do |f|
+      f.change_make_var! "CC", ENV.cc
+      f.change_make_var! "CCOPT_x86", ""
+    end
+
+    ENV.O2 # Respect the developer's choice.
+
+    args = %W[PREFIX=#{prefix}]
+
+    cflags = []
+    cflags << "-DLUAJIT_ENABLE_LUA52COMPAT" if build.with? "52compat"
+    cflags << "-DLUAJIT_ENABLE_GC64" if !build.stable? && build.with?("gc64")
+
+    args << "XCFLAGS=#{cflags.join(" ")}" unless cflags.empty?
+
+    # This doesn't yet work under superenv because it removes "-g"
+    args << "CCDEBUG=-g" if build.with? "debug"
+
+    # The development branch of LuaJIT normally does not install "luajit".
+    args << "INSTALL_TNAME=luajit" if build.devel?
+
+    system "make", "amalg", *args
+    system "make", "install", *args
+
+    # LuaJIT doesn't automatically symlink unversioned libraries:
+    # https://github.com/Homebrew/homebrew/issues/45854.
+    lib.install_symlink lib/"libluajit-5.1.dylib" => "libluajit.dylib"
+    lib.install_symlink lib/"libluajit-5.1.a" => "libluajit.a"
+
+    # Fix path in pkg-config so modules are installed
+    # to permanent location rather than inside the Cellar.
+    inreplace lib/"pkgconfig/luajit.pc" do |s|
+      s.gsub! "INSTALL_LMOD=${prefix}/share/lua/${abiver}",
+              "INSTALL_LMOD=#{HOMEBREW_PREFIX}/share/lua/${abiver}"
+      s.gsub! "INSTALL_CMOD=${prefix}/${multilib}/lua/${abiver}",
+              "INSTALL_CMOD=#{HOMEBREW_PREFIX}/${multilib}/lua/${abiver}"
+      if build.without? "gc64"
+        s.gsub! "Libs:",
+                "Libs: -pagezero_size 10000 -image_base 100000000"
+      end
+    end
+
+    # Having an empty Lua dir in lib/share can mess with other Homebrew Luas.
+    %W[#{lib}/lua #{share}/lua].each { |d| rm_rf d }
+  end
+
+  test do
+    system "#{bin}/luajit", "-e", <<~EOS
+      local ffi = require("ffi")
+      ffi.cdef("int printf(const char *fmt, ...);")
+      ffi.C.printf("Hello %s!\\n", "#{ENV["USER"]}")
+    EOS
+  end
+end
