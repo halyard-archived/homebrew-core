4c8379b6ecc42511d02642822f28a429190208fd
Les Aker <me@lesaker.org>
2017-11-24 19:23:56 -0500

    add jemalloc

---
+++ Formula/jemalloc-halyard.rb
@@ -0,0 +1,55 @@
+class JemallocHalyard < Formula
+  desc "malloc implementation emphasizing fragmentation avoidance"
+  homepage "http://jemalloc.net/"
+  url "https://github.com/jemalloc/jemalloc/releases/download/5.0.1/jemalloc-5.0.1.tar.bz2"
+  sha256 "4814781d395b0ef093b21a08e8e6e0bd3dab8762f9935bbfb71679b0dea7c3e9"
+
+  head do
+    url "https://github.com/jemalloc/jemalloc.git"
+
+    depends_on "autoconf-halyard" => :build
+    depends_on "docbook-xsl-halyard" => :build
+  end
+
+  conflicts_with "jemalloc", :because => "jemalloc-halyard replaces jemalloc"
+
+  def install
+    args = %W[
+      --disable-debug
+      --prefix=#{prefix}
+      --with-jemalloc-prefix=
+    ]
+
+    if build.head?
+      args << "--with-xslroot=#{Formula["docbook-xsl-halyard"].opt_prefix}/docbook-xsl"
+      system "./autogen.sh", *args
+      system "make", "dist"
+    else
+      system "./configure", *args
+    end
+
+    system "make"
+    system "make", "check"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <stdlib.h>
+      #include <jemalloc/jemalloc.h>
+
+      int main(void) {
+
+        for (size_t i = 0; i < 1000; i++) {
+            // Leak some memory
+            malloc(i * 100);
+        }
+
+        // Dump allocator statistics to stderr
+        malloc_stats_print(NULL, NULL, NULL);
+      }
+    EOS
+    system ENV.cc, "test.c", "-L#{lib}", "-ljemalloc", "-o", "test"
+    system "./test"
+  end
+end
