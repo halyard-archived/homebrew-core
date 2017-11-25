03b22ab4c05f8897e7cbd8329cda187bdf6ed556
Les Aker <me@lesaker.org>
2017-11-25 11:26:58 -0500

    add libatomic_ops

---
+++ Formula/libatomic_ops-halyard.rb
@@ -0,0 +1,15 @@
+class LibatomicOpsHalyard < Formula
+  desc "Implementations for atomic memory update operations"
+  homepage "https://github.com/ivmai/libatomic_ops/"
+  url "https://github.com/ivmai/libatomic_ops/releases/download/v7.4.8/libatomic_ops-7.4.8.tar.gz"
+  sha256 "c405d5524b118c197499bc311b8ebe18f7fe47fc820f2c2fcefde1753fbd436a"
+
+  conflicts_with "libatomic_ops", :because => "libatomic_ops-halyard replaces libatomic_ops"
+
+  def install
+    system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
+    system "make"
+    system "make", "check"
+    system "make", "install"
+  end
+end
