cd02e1984a185dbffeb65347261df4514e9b6966
Les Aker <me@lesaker.org>
2017-11-24 19:13:42 -0500

    add libglade

---
+++ Formula/libglade-halyard.rb
@@ -0,0 +1,82 @@
+class LibgladeHalyard < Formula
+  desc "RAD tool to help build GTK+ interfaces"
+  homepage "https://glade.gnome.org"
+  url "https://download.gnome.org/sources/libglade/2.6/libglade-2.6.4.tar.gz"
+  sha256 "c41d189b68457976069073e48d6c14c183075d8b1d8077cb6dfb8b7c5097add3"
+  revision 2
+
+  depends_on "pkg-config-halyard" => :build
+  depends_on "libxml2-halyard"
+  depends_on "gtk+-halyard"
+
+  conflicts_with "libglade", :because => "libglade-halyard replaces libglade"
+
+  def install
+    ENV.append "LDFLAGS", "-lgmodule-2.0"
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <glade/glade.h>
+
+      int main(int argc, char *argv[]) {
+        glade_init();
+        return 0;
+      }
+    EOS
+    ENV.libxml2
+    atk = Formula["atk-halyard"]
+    cairo = Formula["cairo-halyard"]
+    fontconfig = Formula["fontconfig-halyard"]
+    freetype = Formula["freetype-halyard"]
+    gdk_pixbuf = Formula["gdk-pixbuf-halyard"]
+    gettext = Formula["gettext-halyard"]
+    glib = Formula["glib-halyard"]
+    gtkx = Formula["gtk+-halyard"]
+    libpng = Formula["libpng-halyard"]
+    pango = Formula["pango-halyard"]
+    pixman = Formula["pixman-halyard"]
+    flags = %W[
+      -I#{atk.opt_include}/atk-1.0
+      -I#{cairo.opt_include}/cairo
+      -I#{fontconfig.opt_include}
+      -I#{freetype.opt_include}/freetype2
+      -I#{gdk_pixbuf.opt_include}/gdk-pixbuf-2.0
+      -I#{gettext.opt_include}
+      -I#{glib.opt_include}/glib-2.0
+      -I#{glib.opt_lib}/glib-2.0/include
+      -I#{gtkx.opt_include}/gtk-2.0
+      -I#{gtkx.opt_lib}/gtk-2.0/include
+      -I#{include}/libglade-2.0
+      -I#{libpng.opt_include}/libpng16
+      -I#{pango.opt_include}/pango-1.0
+      -I#{pixman.opt_include}/pixman-1
+      -D_REENTRANT
+      -L#{atk.opt_lib}
+      -L#{cairo.opt_lib}
+      -L#{gdk_pixbuf.opt_lib}
+      -L#{gettext.opt_lib}
+      -L#{glib.opt_lib}
+      -L#{gtkx.opt_lib}
+      -L#{lib}
+      -L#{pango.opt_lib}
+      -latk-1.0
+      -lcairo
+      -lgdk-quartz-2.0
+      -lgdk_pixbuf-2.0
+      -lgio-2.0
+      -lglade-2.0
+      -lglib-2.0
+      -lgobject-2.0
+      -lgtk-quartz-2.0
+      -lintl
+      -lpango-1.0
+      -lpangocairo-1.0
+      -lxml2
+    ]
+    system ENV.cc, "test.c", "-o", "test", *flags
+    system "./test"
+  end
+end
