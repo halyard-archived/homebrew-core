8dc924273942e5a1d2caa5787c9f6832162e1a23
Les Aker <me@lesaker.org>
2017-11-30 13:57:32 -0500

    update libunistring

--- Formula/libunistring-halyard.rb
+++ Formula/libunistring-halyard.rb
@@ -1,9 +1,9 @@
 class LibunistringHalyard < Formula
   desc "C string library for manipulating Unicode strings"
   homepage "https://www.gnu.org/software/libunistring/"
-  url "https://ftp.gnu.org/gnu/libunistring/libunistring-0.9.7.tar.xz"
-  mirror "https://ftpmirror.gnu.org/libunistring/libunistring-0.9.7.tar.xz"
-  sha256 "2e3764512aaf2ce598af5a38818c0ea23dedf1ff5460070d1b6cee5c3336e797"
+  url "https://ftp.gnu.org/gnu/libunistring/libunistring-0.9.8.tar.xz"
+  mirror "https://ftpmirror.gnu.org/libunistring/libunistring-0.9.8.tar.xz"
+  sha256 "7b9338cf52706facb2e18587dceda2fbc4a2a3519efa1e15a3f2a68193942f80"
 
   conflicts_with "libunistring", :because => "libunistring-halyard replaces libunistring"
 
