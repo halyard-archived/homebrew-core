1dd07bbae71a7a6f48dfc76c2121f43344adec31
Les Aker <me@lesaker.org>
2017-12-08 17:05:36 -0800

    update pinentry

--- Formula/pinentry-halyard.rb
+++ Formula/pinentry-halyard.rb
@@ -1,9 +1,8 @@
 class PinentryHalyard < Formula
   desc "Passphrase entry dialog utilizing the Assuan protocol"
   homepage "https://www.gnupg.org/related_software/pinentry/"
-  url "https://www.gnupg.org/ftp/gcrypt/pinentry/pinentry-1.0.0.tar.bz2"
-  mirror "https://www.mirrorservice.org/sites/ftp.gnupg.org/gcrypt/pinentry/pinentry-1.0.0.tar.bz2"
-  sha256 "1672c2edc1feb036075b187c0773787b2afd0544f55025c645a71b4c2f79275a"
+  url "https://www.gnupg.org/ftp/gcrypt/pinentry/pinentry-1.1.0.tar.bz2"
+  sha256 "68076686fa724a290ea49cdf0d1c0c1500907d1b759a3bcbfbec0293e8f56570"
 
   depends_on "pkg-config-halyard" => :build
   depends_on "libgpg-error-halyard"
