485cdb52317282d4e3821d552e5b8861c039632d
Les Aker <me@lesaker.org>
2017-12-17 16:11:21 -0500

    add gpatch

---
+++ Formula/gpatch.rb
@@ -0,0 +1,24 @@
+class Gpatch < Formula
+  desc "Apply a diff file to an original"
+  homepage "https://savannah.gnu.org/projects/patch/"
+  url "https://ftp.gnu.org/gnu/patch/patch-2.7.5.tar.xz"
+  sha256 "fd95153655d6b95567e623843a0e77b81612d502ecf78a489a4aed7867caa299"
+
+  def install
+    system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    testfile = testpath/"test"
+    testfile.write "homebrew\n"
+    patch = <<~EOS
+      1c1
+      < homebrew
+      ---
+      > hello
+    EOS
+    pipe_output("#{bin}/patch #{testfile}", patch)
+    assert_equal "hello", testfile.read.chomp
+  end
+end
