f210fd947ea2bc838e70e19fe40db7c55424bd39
Les Aker <me@lesaker.org>
2018-02-03 09:25:24 -0500

    add libu2f-host and hidapi

---
+++ Formula/libu2f-host.rb
@@ -0,0 +1,33 @@
+class Libu2fHost < Formula
+  desc "Host-side of the Universal 2nd Factor (U2F) protocol"
+  homepage "https://developers.yubico.com/libu2f-host/"
+  url "https://developers.yubico.com/libu2f-host/Releases/libu2f-host-1.1.4.tar.xz"
+  sha256 "6043ec020d96358a4887a3ff09492c4f9f6b5bccc48dcdd8f28b15b1c6157a6f"
+
+  depends_on "pkg-config" => :build
+  depends_on "hidapi"
+  depends_on "json-c"
+
+  def install
+    system "./configure", "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <u2f-host.h>
+      int main()
+      {
+        u2fh_devs *devs;
+        u2fh_global_init (0);
+        u2fh_devs_init (&devs);
+        u2fh_devs_discover (devs, NULL);
+        u2fh_devs_done (devs);
+        u2fh_global_done ();
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test.c", "-o", "test", "-I#{include}/u2f-host", "-L#{lib}", "-lu2f-host"
+    system "./test"
+  end
+end
