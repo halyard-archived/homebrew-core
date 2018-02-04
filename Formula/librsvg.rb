8dbf7ac6c5120ce1db04f9b1b0381b8cf144a4b0
Les Aker <me@lesaker.org>
2018-02-04 08:38:15 -0500

    update librsvg

--- Formula/librsvg.rb
+++ Formula/librsvg.rb
@@ -1,8 +1,8 @@
 class Librsvg < Formula
   desc "Library to render SVG files using Cairo"
   homepage "https://live.gnome.org/LibRsvg"
-  url "https://download.gnome.org/sources/librsvg/2.42/librsvg-2.42.0.tar.xz"
-  sha256 "9ab0a728b2d9e6edc561bf35d65054480aee28b278c18b6eb6d0a41a1604461a"
+  url "https://download.gnome.org/sources/librsvg/2.42/librsvg-2.42.2.tar.xz"
+  sha256 "0c550a0bffef768a436286116c03d9f6cd3f97f5021c13e7f093b550fac12562"
 
   depends_on "pkg-config" => :build
   depends_on "cairo"
