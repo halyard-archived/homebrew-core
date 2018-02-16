fae8745a669ff825ae04a9db0371cdb3b7137e96
Les Aker <me@lesaker.org>
2018-02-16 18:41:17 -0500

    add watchexec

---
+++ Formula/watchexec.rb
@@ -0,0 +1,21 @@
+class Watchexec < Formula
+  desc "Execute commands when watched files change"
+  homepage "https://github.com/mattgreen/watchexec"
+  url "https://github.com/mattgreen/watchexec/archive/1.8.6.tar.gz"
+  sha256 "4caa882a17d3e826dca92af157382145c599ac204e9b9ea810dc309402a200e8"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "build", "--release"
+    bin.install "target/release/watchexec"
+    man1.install "doc/watchexec.1"
+  end
+
+  test do
+    o = IO.popen("#{bin}/watchexec -- echo 'saw file change'")
+    sleep 0.1
+    Process.kill("INT", o.pid)
+    assert_match "saw file change", o.read
+  end
+end
