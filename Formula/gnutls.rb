855f372072ac24ba19c25662c44ad148972751d6
Les Aker <me@lesaker.org>
2018-02-23 22:26:18 -0500

    update gnutls

--- Formula/gnutls.rb
+++ Formula/gnutls.rb
@@ -1,8 +1,8 @@
 class Gnutls < Formula
   desc "GNU Transport Layer Security (TLS) Library"
   homepage "https://gnutls.org/"
-  url "https://gnupg.org/ftp/gcrypt/gnutls/v3.5/gnutls-3.5.17.tar.xz"
-  sha256 "86b142afef587c118d63f72ccf307f3321dbc40357aae528202b65d913d20919"
+  url "https://gnupg.org/ftp/gcrypt/gnutls/v3.5/gnutls-3.5.18.tar.xz"
+  sha256 "ae2248d9e78747cf9c469dde81ff8f90b56838b707a0637f3f7d4eee90e80234"
 
   depends_on "pkg-config" => :build
   depends_on "libtasn1"
