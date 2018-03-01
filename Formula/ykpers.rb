1aacf254aaad050234ec3e9a03cb11de016a5527
Les Aker <me@lesaker.org>
2018-03-01 10:23:09 -0500

    add yubi libs

---
+++ Formula/ykpers.rb
@@ -0,0 +1,25 @@
+class Ykpers < Formula
+  desc "YubiKey personalization library and tool"
+  homepage "https://developers.yubico.com/yubikey-personalization/"
+  url "https://developers.yubico.com/yubikey-personalization/Releases/ykpers-1.18.1.tar.gz"
+  sha256 "9ffdb938121a867aa0b350b49daff5807884c2268dfe6d245d133474cd9c5256"
+
+  depends_on "pkg-config" => :build
+  depends_on "libyubikey"
+  depends_on "json-c" => :recommended
+
+  def install
+    libyubikey_prefix = Formula["libyubikey"].opt_prefix
+    system "./configure", "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}",
+                          "--with-libyubikey-prefix=#{libyubikey_prefix}",
+                          "--with-backend=osx"
+    system "make", "check"
+    system "make", "install"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/ykinfo -V 2>&1")
+  end
+end
