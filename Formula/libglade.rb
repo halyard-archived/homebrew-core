e80f46c656671db87b42bd82bf53064353d41db6
Les Aker <me@lesaker.org>
2018-03-06 18:27:43 -0500

    update voyager

--- Formula/libglade.rb
+++ Formula/libglade.rb
@@ -1,7 +1,7 @@
 class Libglade < Formula
   desc "RAD tool to help build GTK+ interfaces"
   homepage "https://glade.gnome.org"
-  url "https://ftp.gnome.org/pub/GNOME/sources/glade/3.20/glade-3.20.3.tar.xz"
+  url "https://ftp.gnome.org/pub/GNOME/sources/glade/3.20/glade-3.20.4.tar.xz"
   sha256 "1739d5478fc54ec8174ff3faa8026053c21c6a10fe38a49c66138f9deada08d4"
   revision 1
 
