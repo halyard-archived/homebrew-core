10475ded1cfe49a4ca00bb8ac4f48c99744df43b
Les Aker <me@lesaker.org>
2018-03-23 16:56:57 +0300

    fix prospectus

---
+++ Formula/aria2.rb
@@ -0,0 +1,38 @@
+class Aria2 < Formula
+  desc "Download with resuming and segmented downloading"
+  homepage "https://aria2.github.io/"
+  url "https://github.com/aria2/aria2/releases/download/release-1.33.1/aria2-1.33.1.tar.xz"
+  sha256 "2539e4844f55a1f1f5c46ad42744335266053a69162e964d9a2d80a362c75e1b"
+
+  depends_on "pkg-config" => :build
+  depends_on "libssh2" => :optional
+
+  needs :cxx11
+
+  def install
+    ENV.cxx11
+
+    args = %W[
+      --disable-dependency-tracking
+      --prefix=#{prefix}
+      --with-appletls
+      --without-openssl
+      --without-gnutls
+      --without-libgmp
+      --without-libnettle
+      --without-libgcrypt
+    ]
+
+    args << "--with-libssh2" if build.with? "libssh2"
+
+    system "./configure", *args
+    system "make", "install"
+
+    bash_completion.install "doc/bash_completion/aria2c"
+  end
+
+  test do
+    system "#{bin}/aria2c", "https://brew.sh/"
+    assert_predicate testpath/"index.html", :exist?, "Failed to create index.html!"
+  end
+end
