f6fe8e82e54c0031ec0c383a1299deb5108d3540
Les Aker <me@lesaker.org>
2018-04-07 20:46:26 -0400

    update pango

--- Formula/pango.rb
+++ Formula/pango.rb
@@ -1,8 +1,8 @@
 class Pango < Formula
   desc "Framework for layout and rendering of i18n text"
   homepage "http://www.pango.org/"
-  url "https://download.gnome.org/sources/pango/1.42/pango-1.42.0.tar.xz"
-  sha256 "9924d88a3dcedff753f0763814a1605307c5c9c931413b8b47ea7267d1b19446"
+  url "https://download.gnome.org/sources/pango/1.42/pango-1.42.1.tar.xz"
+  sha256 "915a6756b298578ff27c7a6393f8c2e62e6e382f9411f2504d7af1a13c7bce32"
 
   head do
     url "https://git.gnome.org/browse/pango.git"
