d2984157227f774dc6ee88b8affec53c2e351fff
Les Aker <me@lesaker.org>
2018-05-28 09:33:50 -0400

    update ncurses

--- Formula/ncurses.rb
+++ Formula/ncurses.rb
@@ -1,8 +1,8 @@
 class Ncurses < Formula
   desc "Text-based UI library"
   homepage "https://www.gnu.org/software/ncurses/"
-  url "https://invisible-mirror.net/archives/ncurses/current/ncurses-6.1-20180519.tgz"
-  sha256 "14d6906e41580ed775514e88af3fbe4c86828b13514e2bf3f3d46dd074a7ada2"
+  url "https://invisible-mirror.net/archives/ncurses/current/ncurses-6.1-20180526.tgz"
+  sha256 "51cec456641d2e03b8981094e2f3bc1fccf79e747e85ac233a5dc8834316f96e"
   revision 1
 
   keg_only :provided_by_macos
