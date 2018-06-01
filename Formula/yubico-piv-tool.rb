837c8accdce83ee9c47154645502dafebd53804e
Les Aker <me@lesaker.org>
2018-06-01 10:14:37 -0400

    update yubico-piv-tool

---
+++ Formula/yubico-piv-tool.rb
@@ -0,0 +1,21 @@
+class YubicoPivTool < Formula
+  desc "Command-line tool for the YubiKey NEO PIV applet"
+  homepage "https://developers.yubico.com/yubico-piv-tool/"
+  url "https://developers.yubico.com/yubico-piv-tool/Releases/yubico-piv-tool-1.5.0.tar.gz"
+  sha256 "c18375179ba25bf9d61365b3903f033f112897bbd54ca63c62fa153f2d05aaab"
+
+  depends_on "check" => :build
+  depends_on "pkg-config" => :build
+  depends_on "openssl"
+
+  def install
+    system "./configure", "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    assert_match "yubico-piv-tool #{version}", shell_output("#{bin}/yubico-piv-tool --version")
+  end
+end
