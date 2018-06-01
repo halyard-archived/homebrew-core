ecde11ef4328b4fe42d5b3f548754d1e010f3320
Les Aker <me@lesaker.org>
2018-06-01 11:29:57 -0400

    fix freetype

--- Formula/freetype.rb
+++ Formula/freetype.rb
@@ -4,19 +4,12 @@ class Freetype < Formula
   url "https://download.savannah.gnu.org/releases/freetype/freetype-2.9.1.tar.bz2"
   sha256 "db8d87ea720ea9d5edc5388fc7a0497bb11ba9fe972245e0f7f4c7e8b1e1e84d"
 
-  option "without-subpixel", "Disable sub-pixel rendering (a.k.a. LCD rendering, or ClearType)"
-
   depends_on "libpng"
 
-
   def install
-    if build.with? "subpixel"
-      inreplace "include/freetype/config/ftoption.h",
-          "/* #define FT_CONFIG_OPTION_SUBPIXEL_RENDERING */",
-          "#define FT_CONFIG_OPTION_SUBPIXEL_RENDERING"
-    end
-
-    system "./configure", "--prefix=#{prefix}", "--without-harfbuzz"
+    system "./configure", "--prefix=#{prefix}",
+                          "--enable-freetype-config",
+                          "--without-harfbuzz"
     system "make"
     system "make", "install"
 
