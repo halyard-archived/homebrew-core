8bcd9748e4c594029ca1923a2bc758b11812932b
Les Aker <me@lesaker.org>
2018-06-21 20:19:58 -0400

    update gdbm

--- Formula/gdbm.rb
+++ Formula/gdbm.rb
@@ -1,9 +1,8 @@
 class Gdbm < Formula
   desc "GNU database manager"
   homepage "https://www.gnu.org/software/gdbm/"
-  url "https://ftp.gnu.org/gnu/gdbm/gdbm-1.14.1.tar.gz"
-  mirror "https://ftpmirror.gnu.org/gdbm/gdbm-1.14.1.tar.gz"
-  sha256 "cdceff00ffe014495bed3aed71c7910aa88bf29379f795abc0f46d4ee5f8bc5f"
+  url "https://ftp.gnu.org/gnu/gdbm/gdbm-1.15.tar.gz"
+  sha256 "f9fde3207f67ed8a5a5ddd8ad5e7acf7b27c2cf0f20dfbdde876dcd6e3d2dc0e"
 
   option "with-libgdbm-compat", "Build libgdbm_compat, a compatibility layer which provides UNIX-like dbm and ndbm interfaces."
 
