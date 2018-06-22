488a02df1f8968b6079f4de38c744a3e435b9bdb
Les Aker <me@lesaker.org>
2018-06-21 20:05:42 -0400

    update jp

--- Formula/jp.rb
+++ Formula/jp.rb
@@ -1,9 +1,9 @@
 class Jp < Formula
   desc "Plot JSON data in the terminal"
   homepage "https://github.com/sgreben/jp"
-  url "https://github.com/sgreben/jp/releases/download/1.1.11/jp_1.1.11_osx_x86_64.zip"
-  version "1.1.11"
-  sha256 "0af7c90ca3c1e70a5f71e7c0ed0e327a10f8bcecaa71b304f340b7d3fbe2d87e"
+  url "https://github.com/sgreben/jp/releases/download/1.1.12/jp_1.1.12_osx_x86_64.zip"
+  version "1.1.12"
+  sha256 "d1cccd9099ccd17203793ff48ec8bab0dfd6edb07c61afd070e9bf281d2453e2"
 
   def install
     bin.install "jp"
