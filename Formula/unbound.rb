5342ccbb0a988e684ed4811bab404a27bf5264a6
Les Aker <me@lesaker.org>
2018-06-21 20:04:28 -0400

    update unbound

--- Formula/unbound.rb
+++ Formula/unbound.rb
@@ -1,8 +1,8 @@
 class Unbound < Formula
   desc "Validating, recursive, caching DNS resolver"
   homepage "https://www.unbound.net"
-  url "https://www.unbound.net/downloads/unbound-1.7.2.tar.gz"
-  sha256 "a85fc7bb34711992cf128b2012638ebb8dc1fe15818baa381f6489240845eaa0"
+  url "https://www.unbound.net/downloads/unbound-1.7.3.tar.gz"
+  sha256 "c11de115d928a6b48b2165e0214402a7a7da313cd479203a7ce7a8b62cba602d"
 
   depends_on "openssl"
   depends_on "libevent"
