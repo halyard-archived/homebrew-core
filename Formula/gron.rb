e2a95c8226ad14b8c5cb9926153fbba67d5edea1
Les Aker <me@lesaker.org>
2018-07-05 18:58:21 -0400

    update gron

--- Formula/gron.rb
+++ Formula/gron.rb
@@ -1,8 +1,8 @@
 class Gron < Formula
   desc "Make JSON greppable"
   homepage "https://github.com/tomnomnom/gron"
-  url "https://github.com/tomnomnom/gron/releases/download/v0.5.2/gron-darwin-amd64-0.5.2.tgz"
-  sha256 "488d928fd743560df4f33bfb380ebb8ade430b93b6ba06fce43f875c35d2f5fb"
+  url "https://github.com/tomnomnom/gron/releases/download/v0.6.0/gron-darwin-amd64-0.6.0.tgz"
+  sha256 "40785d40e1d8b9a634ef7fcbcd471b579ff4dfe870dade595979722aaa1e06a9"
 
   def install
     bin.install "gron"
