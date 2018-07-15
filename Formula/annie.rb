e0e501a7ddeae22fa2f0976bca576ea20c90e93c
Les Aker <me@lesaker.org>
2018-07-14 23:32:44 -0400

    update annie

--- Formula/annie.rb
+++ Formula/annie.rb
@@ -1,9 +1,9 @@
 class Annie < Formula
   desc "Fast, simple and clean video downloader"
   homepage "https://github.com/iawia002/annie"
-  url "https://github.com/iawia002/annie/releases/download/0.7.5/annie_0.7.5_macOS_64-bit.tar.gz"
-  version "0.7.5"
-  sha256 "a12de17c650a0803d6a6b3dc63d8341b95ce2f05ffe51ed656b856d9bece87cf"
+  url "https://github.com/iawia002/annie/releases/download/0.7.8/annie_0.7.8_macOS_64-bit.tar.gz"
+  version "0.7.8"
+  sha256 "cd9f99cfebca1e33ba2ec66af2fdcf5b555b0a2e3b41acd6ad1b2847745f5eee"
 
   def install
     bin.install "annie"
