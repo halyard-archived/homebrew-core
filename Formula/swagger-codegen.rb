46e2298fc2f4d1930e33844d6d8af7fb6f2d22ef
Les Aker <me@lesaker.org>
2018-08-03 11:32:11 -0400

    add maven and swagger

---
+++ Formula/swagger-codegen.rb
@@ -0,0 +1,37 @@
+class SwaggerCodegen < Formula
+  desc "Generate clients, server stubs, and docs from an OpenAPI spec"
+  homepage "https://swagger.io/swagger-codegen/"
+  url "https://github.com/swagger-api/swagger-codegen/archive/v2.3.1.tar.gz"
+  sha256 "0f86c36a5961b0212f3f3b28650d6c6545b281ce1405411edee8505dfbb4073e"
+  head "https://github.com/swagger-api/swagger-codegen.git"
+
+  depends_on :java => "1.8"
+  depends_on "maven" => :build
+
+  def install
+    # Need to set JAVA_HOME manually since maven overrides 1.8 with 1.7+
+    cmd = Language::Java.java_home_cmd("1.8")
+    ENV["JAVA_HOME"] = Utils.popen_read(cmd).chomp
+
+    system "mvn", "clean", "package"
+    libexec.install "modules/swagger-codegen-cli/target/swagger-codegen-cli.jar"
+    bin.write_jar_script libexec/"swagger-codegen-cli.jar", "swagger-codegen"
+  end
+
+  test do
+    (testpath/"minimal.yaml").write <<~EOS
+      ---
+      swagger: '2.0'
+      info:
+        version: 0.0.0
+        title: Simple API
+      paths:
+        /:
+          get:
+            responses:
+              200:
+                description: OK
+    EOS
+    system "#{bin}/swagger-codegen", "generate", "-i", "minimal.yaml", "-l", "swagger"
+  end
+end
