1d6153c9557b50b111c11aa743400978d15f3695
Les Aker <me@lesaker.org>
2018-09-12 10:27:12 -0400

    update fontconfig

--- Formula/fontconfig.rb
+++ Formula/fontconfig.rb
@@ -16,28 +16,14 @@ class Fontconfig < Formula
   depends_on "freetype"
 
   def install
-    # Remove for > 2.13.0
-    # Upstream issue from 6 Mar 2018 "2.13.0 erroneously requires libuuid on macOS"
-    # See https://bugs.freedesktop.org/show_bug.cgi?id=105366
-    ENV["UUID_CFLAGS"] = " "
-    ENV["UUID_LIBS"] = " "
-
-    # Remove for > 2.13.0
-    # Same effect as upstream commit from 10 Mar 2018 "Add uuid to
-    # Requires.private in .pc only when pkgconfig macro found it"
-    inreplace "configure",
-      'PKGCONFIG_REQUIRES_PRIVATELY="$PKGCONFIG_REQUIRES_PRIVATELY uuid"', ""
-
     font_dirs = %w[
       /System/Library/Fonts
       /Library/Fonts
       ~/Library/Fonts
     ]
 
-    if MacOS.version == :sierra
-      font_dirs << "/System/Library/Assets/com_apple_MobileAsset_Font3"
-    elsif MacOS.version >= :high_sierra
-      font_dirs << "/System/Library/Assets/com_apple_MobileAsset_Font4"
+    if MacOS.version >= :sierra
+      font_dirs << Dir["/System/Library/Assets/com_apple_MobileAsset_Font*"].max
     end
 
     system "autoreconf", "-iv" if build.head?
