6298f5bd7dde91c1659fe42de981e997bd93f823
Les Aker <me@lesaker.org>
2018-09-13 11:42:11 -0400

    fix noti formula

--- Formula/noti.rb
+++ Formula/noti.rb
@@ -2,6 +2,7 @@ class Noti < Formula
   desc "Trigger notifications when a process completes"
   homepage "https://github.com/variadico/noti"
   url "https://github.com/variadico/noti/releases/download/3.1.0/noti3.1.0.darwin-amd64.tar.gz"
+  version "3.1.0"
   sha256 "ddfd003f36256c96b75165c30a3cd4e32ec61b33809c677766a25b2161874456"
 
   def install
