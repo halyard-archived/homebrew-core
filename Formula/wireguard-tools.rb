a810c0797400cae825fc41bd83771eacecc47c73
Les Aker <me@lesaker.org>
2018-09-17 19:13:34 -0400

    update wireguard

--- Formula/wireguard-tools.rb
+++ Formula/wireguard-tools.rb
@@ -1,8 +1,8 @@
 class WireguardTools < Formula
   desc "Tools for the WireGuard secure network tunnel"
   homepage "https://www.wireguard.com/"
-  url "https://git.zx2c4.com/WireGuard/snapshot/WireGuard-0.0.20180904.tar.xz"
-  sha256 "a38ead72994a7db7cda2d0085f410df1111b4728db050a519883eda8f3fe38f1"
+  url "https://git.zx2c4.com/WireGuard/snapshot/WireGuard-0.0.20180910.tar.xz"
+  sha256 "43481ac82d4889491e1ae761d4ef10688410975cc861db5d2ac1845ac62eae39"
   head "https://git.zx2c4.com/WireGuard", :using => :git
 
   depends_on "bash"
