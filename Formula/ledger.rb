9555894d0f785855c07478c4cf170430d4b11366
Les Aker <me@lesaker.org>
2018-11-12 23:01:32 -0500

    fix ledger

--- Formula/ledger.rb
+++ Formula/ledger.rb
@@ -3,20 +3,40 @@ class Ledger < Formula
   homepage "https://ledger-cli.org/"
   url "https://github.com/ledger/ledger/archive/v3.1.1.tar.gz"
   sha256 "90f06561ab692b192d46d67bc106158da9c6c6813cc3848b503243a9dfd8548a"
-  revision 21
+  revision 22
   head "https://github.com/ledger/ledger.git"
 
   depends_on "cmake" => :build
   depends_on "boost"
+  depends_on "boost-python"
   depends_on "gmp"
   depends_on "mpfr"
+  depends_on "python@2"
+
+  # Upstream fix for boost 1.68, remove with next version
+  patch do
+    url "https://github.com/ledger/ledger/commit/c18a55f9.diff?full_index=1"
+    sha256 "2e652fc4b247b9c7cd482bd07aa57a66fc86597d7a564e6ccf93232700a6c8d8"
+  end
+
+  needs :cxx11
 
   def install
+    ENV.cxx11
+
+    # Boost >= 1.67 Python components require a Python version suffix
+    inreplace "CMakeLists.txt", "set(BOOST_PYTHON python)",
+                                "set(BOOST_PYTHON python27)"
+
     args = %W[
       --jobs=#{ENV.make_jobs}
       --output=build
       --prefix=#{prefix}
       --boost=#{Formula["boost"].opt_prefix}
+      --python
+      --
+      -DBUILD_DOCS=1
+      -DBUILD_WEB_DOCS=1
     ]
     system "./acprep", "opt", "make", *args
     system "./acprep", "opt", "make", "doc", *args
@@ -24,6 +44,7 @@ class Ledger < Formula
 
     (pkgshare/"examples").install Dir["test/input/*.dat"]
     pkgshare.install "contrib"
+    pkgshare.install "python/demo.py"
     elisp.install Dir["lisp/*.el", "lisp/*.elc"]
     bash_completion.install pkgshare/"contrib/ledger-completion.bash"
   end
@@ -37,5 +58,7 @@ class Ledger < Formula
       "balance", "--collapse", "equity"
     assert_equal "          $-2,500.00  Equity", balance.read.chomp
     assert_equal 0, $CHILD_STATUS.exitstatus
+
+    system "python", pkgshare/"demo.py"
   end
 end
