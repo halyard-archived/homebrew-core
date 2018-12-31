35cf183870d9ad69e6a08fe682bbcc5ac6e8608e
Les Aker <me@lesaker.org>
2018-12-31 11:14:21 -0500

    update sed

--- Formula/gnu-sed.rb
+++ Formula/gnu-sed.rb
@@ -1,9 +1,9 @@
 class GnuSed < Formula
   desc "GNU implementation of the famous stream editor"
   homepage "https://www.gnu.org/software/sed/"
-  url "https://ftp.gnu.org/gnu/sed/sed-4.5.tar.xz"
-  mirror "https://ftpmirror.gnu.org/sed/sed-4.5.tar.xz"
-  sha256 "7aad73c8839c2bdadca9476f884d2953cdace9567ecd0d90f9959f229d146b40"
+  url "https://ftp.gnu.org/gnu/sed/sed-4.7.tar.xz"
+  mirror "https://ftpmirror.gnu.org/sed/sed-4.7.tar.xz"
+  sha256 "2885768cd0a29ff8d58a6280a270ff161f6a3deb5690b2be6c49f46d4c67bd6a"
 
   option "with-default-names", "Do not prepend 'g' to the binary"
 
