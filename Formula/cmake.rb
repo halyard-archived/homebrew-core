962788a3e4e3085f8b6a12eac11128070507c92f
Les Aker <me@lesaker.org>
2018-12-31 11:07:52 -0500

    update cmake

--- Formula/cmake.rb
+++ Formula/cmake.rb
@@ -1,8 +1,8 @@
 class Cmake < Formula
   desc "Cross-platform make"
   homepage "https://www.cmake.org/"
-  url "https://cmake.org/files/v3.12/cmake-3.12.4.tar.gz"
-  sha256 "5255584bfd043eb717562cff8942d472f1c0e4679c4941d84baadaa9b28e3194"
+  url "https://cmake.org/files/v3.13/cmake-3.13.2.tar.gz"
+  sha256 "c925e7d2c5ba511a69f43543ed7b4182a7d446c274c7480d0e42cd933076ae25"
   head "https://cmake.org/cmake.git"
 
   option "without-docs", "Don't build man pages"
