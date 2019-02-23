47ff3b49057f830a1bed710890b5a01d877c771f
Les Aker <me@lesaker.org>
2019-02-24 00:07:00 +0300

    update libusb to remove options

--- Formula/libusb.rb
+++ Formula/libusb.rb
@@ -1,25 +1,11 @@
 class Libusb < Formula
   desc "Library for USB device access"
-  homepage "https://libusb.info"
+  homepage "https://libusb.info/"
   url "https://github.com/libusb/libusb/releases/download/v1.0.22/libusb-1.0.22.tar.bz2"
   sha256 "75aeb9d59a4fdb800d329a545c2e6799f732362193b465ea198f2aa275518157"
 
-  head do
-    url "https://github.com/libusb/libusb.git"
-
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-    depends_on "libtool" => :build
-  end
-
-  option "without-runtime-logging", "Build without runtime logging functionality"
-  option "with-default-log-level-debug", "Build with default runtime log level of debug (instead of none)"
-
-
   def install
     args = %W[--disable-dependency-tracking --prefix=#{prefix}]
-    args << "--disable-log" if build.without? "runtime-logging"
-    args << "--enable-debug-log" if build.with? "default-log-level-debug"
 
     system "./autogen.sh" if build.head?
     system "./configure", *args
