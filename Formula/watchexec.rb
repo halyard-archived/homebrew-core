7b0804afe52387b40e9e23968d30e21a4654d93f
Les Aker <me@lesaker.org>
2019-02-23 22:38:55 +0300

    update watchexec

--- Formula/watchexec.rb
+++ Formula/watchexec.rb
@@ -1,8 +1,8 @@
 class Watchexec < Formula
   desc "Execute commands when watched files change"
   homepage "https://github.com/mattgreen/watchexec"
-  url "https://github.com/mattgreen/watchexec/releases/download/1.9.2/watchexec-1.9.2-x86_64-apple-darwin.tar.gz"
-  sha256 "0e4765a01c994360917766e0d2bc9b6a8f6e5e190a37806d2c2552bc85ac7c04"
+  url "https://github.com/mattgreen/watchexec/releases/download/1.10.1/watchexec-1.10.1-x86_64-apple-darwin.tar.gz"
+  sha256 "9f3e572200d516024697144d64612c89e3fce610b579251dc783dbeae07ac671"
 
   def install
     bin.install "watchexec"
