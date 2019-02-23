052458fa3e658eaaf3dc1cd7c55669ea87f38219
Les Aker <me@lesaker.org>
2019-02-24 00:22:55 +0300

    update ghc

--- Formula/ghc.rb
+++ Formula/ghc.rb
@@ -5,44 +5,61 @@ class Ghc < Formula
 
   desc "Glorious Glasgow Haskell Compilation System"
   homepage "https://haskell.org/ghc/"
-  url "https://downloads.haskell.org/~ghc/8.6.2/ghc-8.6.2-src.tar.xz"
-  sha256 "caaa819d21280ecde90a4773143dee188711e9ff175a27cfbaee56eb851d76d5"
+  url "https://downloads.haskell.org/~ghc/8.6.3/ghc-8.6.3-src.tar.xz"
+  sha256 "9f9e37b7971935d88ba80426c36af14b1e0b3ec1d9c860f44a4391771bc07f23"
 
-  option "with-test", "Verify the build using the testsuite"
-  option "without-docs", "Do not build documentation (including man page)"
+  depends_on "python" => :build
+  depends_on "sphinx-doc" => :build
 
-  depends_on "python" => :build if build.with?("test")
-  depends_on "sphinx-doc" => :build if build.with? "docs"
-  depends_on "gmp"
+  resource "gmp" do
+    url "https://ftp.gnu.org/gnu/gmp/gmp-6.1.2.tar.xz"
+    mirror "https://gmplib.org/download/gmp/gmp-6.1.2.tar.xz"
+    mirror "https://ftpmirror.gnu.org/gmp/gmp-6.1.2.tar.xz"
+    sha256 "87b565e89a9a684fe4ebeeddb8399dce2599f9c9049854ca8c0dfbdea0e21912"
+  end
 
   # https://www.haskell.org/ghc/download_ghc_8_0_1#macosx_x86_64
   # "This is a distribution for Mac OS X, 10.7 or later."
   resource "binary" do
-    url "https://downloads.haskell.org/~ghc/8.4.3/ghc-8.4.3-x86_64-apple-darwin.tar.xz"
-    sha256 "af0b455f6c46b9802b4b48dad996619cfa27cc6e2bf2ce5532387b4a8c00aa64"
-  end
-
-  resource "testsuite" do
-    url "https://downloads.haskell.org/~ghc/8.4.3/ghc-8.4.3-testsuite.tar.xz"
-    sha256 "ff43a015f803005dd9d9248ea9ffa92f9ebe79e146cfd044c3f48e0a7e58a5fc"
+    url "https://downloads.haskell.org/~ghc/8.4.4/ghc-8.4.4-x86_64-apple-darwin.tar.xz"
+    sha256 "28dc89ebd231335337c656f4c5ead2ae2a1acc166aafe74a14f084393c5ef03a"
   end
 
   def install
     ENV["CC"] = ENV.cc
     ENV["LD"] = "ld"
-    args = []
 
-    # Setting -march=native, which is what --build-from-source does, fails
-    # on Skylake (and possibly other architectures as well) with the error
-    # "Segmentation fault: 11" for at least the following files:
-    #   utils/haddock/dist/build/Haddock/Backends/Hyperlinker/Types.dyn_o
-    #   utils/haddock/dist/build/Documentation/Haddock/Types.dyn_o
-    #   utils/haddock/dist/build/Haddock/GhcUtils.dyn_o
-    #   utils/haddock/dist/build/Paths_haddock.dyn_o
-    #   utils/haddock/dist/build/ResponseFile.dyn_o
-    # Setting -march=core2 works around the bug.
-    # Reported 22 May 2016: https://ghc.haskell.org/trac/ghc/ticket/12100
-    ENV["HOMEBREW_OPTFLAGS"] = "-march=#{Hardware.oldest_cpu}"
+    # Build a static gmp rather than in-tree gmp, otherwise all ghc-compiled
+    # executables link to Homebrew's GMP.
+    gmp = libexec/"integer-gmp"
+
+    # GMP *does not* use PIC by default without shared libs so --with-pic
+    # is mandatory or else you'll get "illegal text relocs" errors.
+    resource("gmp").stage do
+      system "./configure", "--prefix=#{gmp}", "--with-pic", "--disable-shared",
+                            "--build=#{Hardware.oldest_cpu}-apple-darwin#{`uname -r`.to_i}"
+      system "make"
+      system "make", "check"
+      system "make", "install"
+    end
+
+    args = ["--with-gmp-includes=#{gmp}/include",
+            "--with-gmp-libraries=#{gmp}/lib"]
+
+    # As of Xcode 7.3 (and the corresponding CLT) `nm` is a symlink to `llvm-nm`
+    # and the old `nm` is renamed `nm-classic`. Building with the new `nm`, a
+    # segfault occurs with the following error:
+    #   make[1]: * [compiler/stage2/dll-split.stamp] Segmentation fault: 11
+    # Upstream is aware of the issue and is recommending the use of nm-classic
+    # until Apple restores POSIX compliance:
+    # https://ghc.haskell.org/trac/ghc/ticket/11744
+    # https://ghc.haskell.org/trac/ghc/ticket/11823
+    # https://mail.haskell.org/pipermail/ghc-devs/2016-April/011862.html
+    # LLVM itself has already fixed the bug: llvm-mirror/llvm@ae7cf585
+    # rdar://25311883 and rdar://25299678
+    if DevelopmentTools.clang_build_version >= 703 && DevelopmentTools.clang_build_version < 800
+      args << "--with-nm=#{`xcrun --find nm-classic`.chomp}"
+    end
 
     resource("binary").stage do
       binary = buildpath/"binary"
@@ -53,16 +70,24 @@ class Ghc < Formula
       ENV.prepend_path "PATH", binary/"bin"
     end
 
+    if build.head?
+      resource("cabal").stage do
+        system "sh", "bootstrap.sh", "--sandbox"
+        (buildpath/"bootstrap-tools/bin").install ".cabal-sandbox/bin/cabal"
+      end
+
+      ENV.prepend_path "PATH", buildpath/"bootstrap-tools/bin"
+
+      cabal_sandbox do
+        cabal_install "--only-dependencies", "happy", "alex"
+        cabal_install "--prefix=#{buildpath}/bootstrap-tools", "happy", "alex"
+      end
+
+      system "./boot"
+    end
+
     system "./configure", "--prefix=#{prefix}", *args
     system "make"
-
-    if build.with?("test")
-      resource("testsuite").stage { buildpath.install Dir["*"] }
-      cd "testsuite" do
-        system "make", "clean"
-        system "make", "CLEANUP=1", "THREADS=#{ENV.make_jobs}", "fast"
-      end
-    end
 
     ENV.deparallelize { system "make", "install" }
     Dir.glob(lib/"*/package.conf.d/package.cache") { |f| rm f }
