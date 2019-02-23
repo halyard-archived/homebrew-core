16fe1ed62cb7045d42dee1d93265c1c5d174897d
Les Aker <me@lesaker.org>
2019-02-24 00:00:41 +0300

    update wget

--- Formula/wget.rb
+++ Formula/wget.rb
@@ -2,39 +2,23 @@ class Wget < Formula
   desc "Internet file retriever"
   homepage "https://www.gnu.org/software/wget/"
   url "https://ftp.gnu.org/gnu/wget/wget-1.20.1.tar.gz"
+  mirror "https://ftpmirror.gnu.org/wget/wget-1.20.1.tar.gz"
   sha256 "b783b390cb571c837b392857945f5a1f00ec6b043177cc42abb8ee1b542ee1b3"
 
-  head do
-    url "https://git.savannah.gnu.org/git/wget.git"
-
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-    depends_on "xz" => :build
-    depends_on "gettext"
-  end
-
-  option "with-debug", "Build with debug support"
-
   depends_on "pkg-config" => :build
+  depends_on "libidn2"
   depends_on "openssl"
-  depends_on "gpgme" => :optional
-  depends_on "pcre" => :optional
-
 
   def install
-    args = %W[
-      --prefix=#{prefix}
-      --sysconfdir=#{etc}
-      --with-ssl=openssl
-      --with-libssl-prefix=#{Formula["openssl"].opt_prefix}
-    ]
-
-    args << "--disable-debug" if build.without? "debug"
-    args << "--disable-pcre" if build.without? "pcre"
-    args << "--with-gpgme-prefix=#{Formula["gpgme"].opt_prefix}" if build.with? "gpgme"
-
-    system "./bootstrap" if build.head?
-    system "./configure", *args
+    system "./bootstrap", "--skip-po" if build.head?
+    system "./configure", "--prefix=#{prefix}",
+                          "--sysconfdir=#{etc}",
+                          "--with-ssl=openssl",
+                          "--with-libssl-prefix=#{Formula["openssl"].opt_prefix}",
+                          "--disable-debug",
+                          "--disable-pcre",
+                          "--disable-pcre2",
+                          "--without-libpsl"
     system "make", "install"
   end
 
