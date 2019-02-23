b5cffd3a794e8ebe5723678981e1d665254aa1f4
Les Aker <me@lesaker.org>
2019-02-24 00:02:24 +0300

    remove options from tcl-tk

--- Formula/tcl-tk.rb
+++ Formula/tcl-tk.rb
@@ -1,24 +1,36 @@
 class TclTk < Formula
   desc "Tool Command Language"
   homepage "https://www.tcl.tk/"
-
   url "https://downloads.sourceforge.net/project/tcl/Tcl/8.6.9/tcl8.6.9-src.tar.gz"
+  mirror "https://ftp.osuosl.org/pub/blfs/conglomeration/tcl/tcl8.6.9-src.tar.gz"
   version "8.6.9"
   sha256 "ad0cd2de2c87b9ba8086b43957a0de3eb2eb565c7159d5f53ccbba3feb915f4e"
 
+  keg_only :provided_by_macos,
+    "tk installs some X11 headers and macOS provides an (older) Tcl/Tk"
 
-  option "without-tcllib", "Don't build tcllib (utility modules)"
-  option "without-tk", "Don't build the Tk (window toolkit)"
+  depends_on "openssl"
 
-  resource "tk" do
-    url "https://downloads.sourceforge.net/project/tcl/Tcl/8.6.9/tk8.6.9-src.tar.gz"
-    version "8.6.9"
-    sha256 "d3f9161e8ba0f107fe8d4df1f6d3a14c30cc3512dfc12a795daa367a27660dac"
+  resource "critcl" do
+    url "https://github.com/andreas-kupries/critcl/archive/3.1.17.tar.gz"
+    sha256 "fff83b341fc07b8ff23bf1f645133bb4bffe4741da2e6f31155e522a74c228e4"
   end
 
   resource "tcllib" do
     url "https://downloads.sourceforge.net/project/tcllib/tcllib/1.19/tcllib-1.19.tar.gz"
     sha256 "01fe87cf1855b96866cf5394b6a786fd40b314022714b34110aeb6af545f6a9c"
+  end
+
+  resource "tcltls" do
+    url "https://core.tcl.tk/tcltls/uv/tcltls-1.7.16.tar.gz"
+    sha256 "6845000732bedf764e78c234cee646f95bb68df34e590c39434ab8edd6f5b9af"
+  end
+
+  resource "tk" do
+    url "https://downloads.sourceforge.net/project/tcl/Tcl/8.6.9/tk8.6.9.1-src.tar.gz"
+    mirror "https://fossies.org/linux/misc/tk8.6.9.1-src.tar.gz"
+    version "8.6.9.1"
+    sha256 "8fcbcd958a8fd727e279f4cac00971eee2ce271dc741650b1fc33375fb74ebb4"
   end
 
   def install
@@ -37,27 +49,36 @@ class TclTk < Formula
       ln_s bin/"tclsh#{version.to_f}", bin/"tclsh"
     end
 
-    if build.with? "tk"
-      ENV.prepend_path "PATH", bin # so that tk finds our new tclsh
+    # Let tk finds our new tclsh
+    ENV.prepend_path "PATH", bin
 
-      resource("tk").stage do
-        cd "unix" do
-          system "./configure", *args, "--enable-aqua=yes",
-                                "--without-x", "--with-tcl=#{lib}"
-          system "make"
-          system "make", "install"
-          system "make", "install-private-headers"
-          ln_s bin/"wish#{version.to_f}", bin/"wish"
-        end
+    resource("tk").stage do
+      cd "unix" do
+        system "./configure", *args, "--enable-aqua=yes",
+                              "--without-x", "--with-tcl=#{lib}"
+        system "make"
+        system "make", "install"
+        system "make", "install-private-headers"
+        ln_s bin/"wish#{version.to_f}", bin/"wish"
       end
     end
 
-    if build.with? "tcllib"
-      resource("tcllib").stage do
-        system "./configure", "--prefix=#{prefix}",
-                              "--mandir=#{man}"
-        system "make", "install"
-      end
+    resource("critcl").stage do
+      system bin/"tclsh", "build.tcl", "install"
+    end
+
+    resource("tcllib").stage do
+      system "./configure", "--prefix=#{prefix}", "--mandir=#{man}"
+      system "make", "install"
+      ENV["SDKROOT"] = MacOS.sdk_path
+      system "make", "critcl"
+      cp_r "modules/tcllibc", "#{lib}/"
+      ln_s "#{lib}/tcllibc/macosx-x86_64-clang", "#{lib}/tcllibc/macosx-x86_64"
+    end
+
+    resource("tcltls").stage do
+      system "./configure", "--with-ssl=openssl", "--with-openssl-dir=#{Formula["openssl"].opt_prefix}", "--prefix=#{prefix}", "--mandir=#{man}"
+      system "make", "install"
     end
   end
 
