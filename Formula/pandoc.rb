f4e01966844f2efde1d2ca3919c23b4c36cbe933
Les Aker <me@lesaker.org>
2019-02-23 23:16:39 +0300

    update pandoc

--- Formula/pandoc.rb
+++ Formula/pandoc.rb
@@ -5,19 +5,15 @@ class Pandoc < Formula
 
   desc "Swiss-army knife of markup format conversion"
   homepage "https://pandoc.org/"
-  _version = '2.5'
-  url "https://hackage.haskell.org/package/pandoc-#{_version}/pandoc-#{_version}.tar.gz"
-  sha256 "d57dc5db78a0a304de70436fe59a7599ab0c6d0fb2ab6704eeae498a4536222e"
-  head "https://github.com/jgm/pandoc.git"
+  url "https://hackage.haskell.org/package/pandoc-2.6/pandoc-2.6.tar.gz"
+  sha256 "aa761fe96161d042e01d0881fa569ec396da02ebce42562e04fbd91d8ff2db10"
 
   depends_on "cabal-install" => :build
   depends_on "ghc" => :build
 
   def install
     cabal_sandbox do
-      args = []
-      args << "--constraint=cryptonite -support_aesni" if MacOS.version <= :lion
-      install_cabal_package *args
+      install_cabal_package
     end
     (bash_completion/"pandoc").write `#{bin}/pandoc --bash-completion`
   end
