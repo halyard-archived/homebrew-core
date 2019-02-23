a59dc94f039dbe337d9b60653a8cb6a7e2bac840
Les Aker <me@lesaker.org>
2019-02-23 22:37:42 +0300

    update weechat

--- Formula/weechat.rb
+++ Formula/weechat.rb
@@ -1,22 +1,20 @@
 class Weechat < Formula
   desc "Extensible IRC client"
   homepage "https://www.weechat.org"
-  url "https://weechat.org/files/src/weechat-2.3.tar.xz"
-  sha256 "ef8654313bfb0ca92e27cf579efb2d9b17e53505e615bf3d71a51aef44e56a5f"
+  url "https://weechat.org/files/src/weechat-2.4.tar.xz"
+  sha256 "86d626c58d666ca58ef4762ff60c24c89f4632d95ed401773d5584043d865c73"
 
-  option "with-perl", "Build the perl module"
-  option "with-ruby", "Build the ruby module"
-  option "with-curl", "Build with brewed curl"
-
+  depends_on "asciidoctor" => :build
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
+  depends_on "aspell"
   depends_on "gettext"
   depends_on "gnutls"
   depends_on "libgcrypt"
-  depends_on "curl" => :optional
-  depends_on "lua" => :optional
-  depends_on "perl" => :optional
-  depends_on "python@2" => :optional
+  depends_on "lua"
+  depends_on "ncurses"
+  depends_on "perl"
+  depends_on "python@2"
 
   def install
     args = std_cmake_args + %W[
@@ -24,19 +22,12 @@ class Weechat < Formula
       -DENABLE_GUILE=OFF
       -DCA_FILE=#{etc}/openssl/cert.pem
       -DENABLE_JAVASCRIPT=OFF
-      -DENABLE_ASPELL=OFF
     ]
 
-    if build.without? "ruby"
-      args << "-DENABLE_RUBY=OFF"
-    elsif build.with?("ruby")
+    if MacOS.version >= :sierra
       args << "-DRUBY_EXECUTABLE=/usr/bin/ruby"
       args << "-DRUBY_LIB=/usr/lib/libruby.dylib"
     end
-
-    args << "-DENABLE_LUA=OFF" if build.without? "lua"
-    args << "-DENABLE_PERL=OFF" if build.without? "perl"
-    args << "-DENABLE_PYTHON=OFF" if build.without? "python@2"
 
     mkdir "build" do
       system "cmake", "..", *args
