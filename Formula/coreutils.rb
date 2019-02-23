0bf183f09bf60c10c43f939c4fb34678f61a1ed3
Les Aker <me@lesaker.org>
2019-02-24 01:18:28 +0300

    update coreutils

--- Formula/coreutils.rb
+++ Formula/coreutils.rb
@@ -2,23 +2,15 @@ class Coreutils < Formula
   desc "GNU File, Shell, and Text utilities"
   homepage "https://www.gnu.org/software/coreutils"
   url "https://ftp.gnu.org/gnu/coreutils/coreutils-8.30.tar.xz"
+  mirror "https://ftpmirror.gnu.org/coreutils/coreutils-8.30.tar.xz"
   sha256 "e831b3a86091496cdba720411f9748de81507798f6130adeaef872d206e1b057"
 
-  depends_on "gmp" => :optional
-
-  #conflicts_with "ganglia", :because => "both install `gstat` binaries"
-  #conflicts_with "gegl", :because => "both install `gcut` binaries"
-  #conflicts_with "idutils", :because => "both install `gid` and `gid.1`"
-  #conflicts_with "aardvark_shell_utils", :because => "both install `realpath` binaries"
-
   def install
-    system "./bootstrap" if build.head?
-
     args = %W[
       --prefix=#{prefix}
       --program-prefix=g
+      --without-gmp
     ]
-    args << "--without-gmp" if build.without? "gmp"
     system "./configure", *args
     system "make", "install"
 
@@ -30,32 +22,32 @@ class Coreutils < Formula
     coreutils_filenames(man1).each do |cmd|
       (libexec/"gnuman"/"man1").install_symlink man1/"g#{cmd}" => cmd
     end
+    libexec.install_symlink "gnuman" => "man"
 
     # Symlink non-conflicting binaries
-    bin.install_symlink "grealpath" => "realpath"
-    man1.install_symlink "grealpath.1" => "realpath.1"
+    no_conflict = %w[
+      b2sum base32 chcon hostid md5sum nproc numfmt pinky ptx realpath runcon
+      sha1sum sha224sum sha256sum sha384sum sha512sum shred shuf stdbuf tac timeout truncate
+    ]
+    no_conflict.each do |cmd|
+      bin.install_symlink "g#{cmd}" => cmd
+      man1.install_symlink "g#{cmd}.1" => "#{cmd}.1"
+    end
   end
 
   def caveats; <<~EOS
-    All commands have been installed with the prefix 'g'.
-
-    If you really need to use these commands with their normal names, you
+    Commands also provided by macOS have been installed with the prefix "g".
+    If you need to use these commands with their normal names, you
     can add a "gnubin" directory to your PATH from your bashrc like:
-
-        PATH="#{opt_libexec}/gnubin:$PATH"
-
-    Additionally, you can access their man pages with normal names if you add
-    the "gnuman" directory to your MANPATH from your bashrc as well:
-
-        MANPATH="#{opt_libexec}/gnuman:$MANPATH"
-
-    EOS
+      PATH="#{opt_libexec}/gnubin:$PATH"
+  EOS
   end
 
   def coreutils_filenames(dir)
     filenames = []
     dir.find do |path|
       next if path.directory? || path.basename.to_s == ".DS_Store"
+
       filenames << path.basename.to_s.sub(/^g/, "")
     end
     filenames.sort
