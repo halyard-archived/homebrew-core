cb59f01f40c9757b2b6455c1a520e061b97d417f
Les Aker <me@lesaker.org>
2019-02-24 00:05:02 +0300

    remove options from nmap

--- Formula/nmap.rb
+++ Formula/nmap.rb
@@ -3,11 +3,11 @@ class Nmap < Formula
   homepage "https://nmap.org/"
   url "https://nmap.org/dist/nmap-7.70.tar.bz2"
   sha256 "847b068955f792f4cc247593aca6dc3dc4aae12976169873247488de147a6e18"
-
-  option "with-pygtk", "Build Zenmap GUI"
+  head "https://svn.nmap.org/nmap/"
 
   depends_on "openssl"
-  depends_on "pygtk" => :optional
+
+  conflicts_with "ndiff", :because => "both install `ndiff` binaries"
 
   def install
     ENV.deparallelize
@@ -19,13 +19,14 @@ class Nmap < Formula
       --with-openssl=#{Formula["openssl"].opt_prefix}
       --without-nmap-update
       --disable-universal
+      --without-zenmap
     ]
-
-    args << "--without-zenmap" if build.without? "pygtk"
 
     system "./configure", *args
     system "make" # separate steps required otherwise the build fails
     system "make", "install"
+
+    rm_f Dir[bin/"uninstall_*"] # Users should use brew uninstall.
   end
 
   test do
