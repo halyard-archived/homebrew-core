9b52b4d82a7cd1e92b8bc747f941e0ed4073edb8
Les Aker <me@lesaker.org>
2019-02-23 22:59:39 +0300

    set dapper version

--- Formula/dapper.rb
+++ Formula/dapper.rb
@@ -3,6 +3,7 @@ class Dapper < Formula
   homepage "https://github.com/akerl/dapper"
 
   url "https://github.com/akerl/dapper/releases/download/v0.1.0/dapper_darwin"
+  version "0.1.0"
   sha256 "28df222019eac45b6a90670b71a826a0f3d4ac1cd8c94007f190c442c16fdcb6"
 
   head do
