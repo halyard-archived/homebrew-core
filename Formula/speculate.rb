8a6245bd52d59c48d0903179f6d8d7f638656d7e
Les Aker <me@lesaker.org>
2019-02-23 23:52:46 +0300

    update swagger-codegen

--- Formula/speculate.rb
+++ Formula/speculate.rb
@@ -3,6 +3,7 @@ class Speculate < Formula
   homepage "https://github.com/akerl/speculate"
 
   url "https://github.com/akerl/speculate/releases/download/v0.10.2/speculate_darwin"
+  version "0.10.2"
   sha256 "0d5bf40de13c232a788eb231b60ade72b53c6bd89255da6b38368c4b85f0d5d7"
 
   head do
