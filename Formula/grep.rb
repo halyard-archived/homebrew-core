1f78291b24a676fb85b5a3b8c32aee69c841da92
Les Aker <me@lesaker.org>
2019-02-24 00:07:39 +0300

    update grep to remove flags

--- Formula/grep.rb
+++ Formula/grep.rb
@@ -5,8 +5,6 @@ class Grep < Formula
   mirror "https://ftpmirror.gnu.org/grep/grep-3.3.tar.xz"
   sha256 "b960541c499619efd6afe1fa795402e4733c8e11ebf9fafccc0bb4bccdc5b514"
 
-  option "with-default-names", "Do not prepend 'g' to the binary"
-
   depends_on "pkg-config" => :build
   depends_on "pcre"
 
@@ -18,29 +16,37 @@ class Grep < Formula
       --infodir=#{info}
       --mandir=#{man}
       --with-packager=Homebrew
+      --program-prefix=g
     ]
-
-    args << "--program-prefix=g" if build.without? "default-names"
 
     system "./configure", *args
     system "make"
     system "make", "install"
+
+    %w[grep egrep fgrep].each do |prog|
+      (libexec/"gnubin").install_symlink bin/"g#{prog}" => prog
+      (libexec/"gnuman/man1").install_symlink man1/"g#{prog}.1" => "#{prog}.1"
+    end
+
+    libexec.install_symlink "gnuman" => "man"
   end
 
-  def caveats
-    if build.without? "default-names" then <<~EOS
-      The command has been installed with the prefix "g".
-      If you do not want the prefix, install using the "with-default-names"
-      option.
-      EOS
-    end
+  def caveats; <<~EOS
+    All commands have been installed with the prefix "g".
+    If you need to use these commands with their normal names, you
+    can add a "gnubin" directory to your PATH from your bashrc like:
+      PATH="#{opt_libexec}/gnubin:$PATH"
+  EOS
   end
 
   test do
     text_file = testpath/"file.txt"
     text_file.write "This line should be matched"
-    cmd = build.with?("default-names") ? "grep" : "ggrep"
-    grepped = shell_output("#{bin}/#{cmd} match #{text_file}")
+
+    grepped = shell_output("#{bin}/ggrep match #{text_file}")
+    assert_match "should be matched", grepped
+
+    grepped = shell_output("#{opt_libexec}/gnubin/grep match #{text_file}")
     assert_match "should be matched", grepped
   end
 end
