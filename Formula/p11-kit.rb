7aacb94bc4624543cda042fe85da01d94edd3868
Les Aker <me@lesaker.org>
2019-02-23 23:15:57 +0300

    update p11-kit

--- Formula/p11-kit.rb
+++ Formula/p11-kit.rb
@@ -1,35 +1,15 @@
 class P11Kit < Formula
   desc "Library to load and enumerate PKCS#11 modules"
   homepage "https://p11-glue.freedesktop.org"
-  url "https://github.com/p11-glue/p11-kit/releases/download/0.23.14/p11-kit-0.23.14.tar.gz"
-  sha256 "1cb9fa6d237539f25f62f4c3d4ec71a1c8e0772957ec45ec5af92134129e0d70"
-
-  head do
-    url "https://github.com/p11-glue/p11-kit.git"
-
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-    depends_on "gettext" => :build
-    depends_on "libtool" => :build
-  end
+  url "https://github.com/p11-glue/p11-kit/releases/download/0.23.15/p11-kit-0.23.15.tar.gz"
+  sha256 "f7c139a0c77a1f0012619003e542060ba8f94799a0ef463026db390680e4d798"
 
   depends_on "pkg-config" => :build
   depends_on "libffi"
 
   def install
-    # Fix "error: unknown type name 'locale_t'"
-    # Reported 25 May 2018 https://github.com/p11-glue/p11-kit/issues/158
-    inreplace %w[common/debug.c common/library.c common/message.c
-                 common/test-message.c],
-              "#include <locale.h>", "#include <xlocale.h>"
-
     # https://bugs.freedesktop.org/show_bug.cgi?id=91602#c1
     ENV["FAKED_MODE"] = "1"
-
-    if build.head?
-      ENV["NOCONFIGURE"] = "1"
-      system "./autogen.sh"
-    end
 
     system "./configure", "--disable-dependency-tracking",
                           "--disable-silent-rules",
