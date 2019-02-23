f906f6711e741f6636501c4e4128851626ff225d
Les Aker <me@lesaker.org>
2019-02-23 23:49:25 +0300

    fix unzip version

--- Formula/unzip.rb
+++ Formula/unzip.rb
@@ -2,9 +2,8 @@ class Unzip < Formula
   desc "Extraction utility for .zip compressed archives"
   homepage "https://infozip.sourceforge.io/UnZip.html"
   url "https://downloads.sourceforge.net/project/infozip/UnZip%206.x%20%28latest%29/UnZip%206.0/unzip60.tar.gz"
-  version "6.0-21"
+  version "6.0-22"
   sha256 "036d96991646d0449ed0aa952e4fbe21b476ce994abc276e49d30e686708bd37"
-  revision 2
 
   keg_only :provided_by_macos
 
