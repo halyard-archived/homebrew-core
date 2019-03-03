00aeaf8b2b4973dfa1f1333daf57ae957b729d96
Les Aker <me@lesaker.org>
2019-03-03 16:05:19 +0300

    fix cop

--- Formula/pick.rb
+++ Formula/pick.rb
@@ -2,7 +2,6 @@ class Pick < Formula
   desc "Command line arg picker"
   homepage "https://github.com/thoughtbot/pick"
   url "https://github.com/thoughtbot/pick/releases/download/v3.0.0/pick-3.0.0.tar.gz"
-  version "3.0.0"
   sha256 "2783b9c11b54301ce32c0c2e70cf4dc8d437cb68cf136ba204a6065422055298"
 
 
