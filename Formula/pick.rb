4422e4f2af26603da2c803ee45158a6e004754ec
Les Aker <me@lesaker.org>
2019-03-03 12:12:17 +0300

    update pick

--- Formula/pick.rb
+++ Formula/pick.rb
@@ -1,9 +1,9 @@
 class Pick < Formula
   desc "Command line arg picker"
   homepage "https://github.com/thoughtbot/pick"
-  version = "2.0.2"
-  url "https://github.com/thoughtbot/pick/releases/download/v#{version}/pick-#{version}.tar.gz"
-  sha256 "f2b43aaa540ad3ff05a256a531c2f47d3d95145b82c1d1b0d62dfb40d793d385"
+  url "https://github.com/thoughtbot/pick/releases/download/v3.0.0/pick-3.0.0.tar.gz"
+  version "3.0.0"
+  sha256 "2783b9c11b54301ce32c0c2e70cf4dc8d437cb68cf136ba204a6065422055298"
 
 
   def install
