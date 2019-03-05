b340633558ee6b1f34b32a091585c2b4e07cc4cb
Les Aker <me@lesaker.org>
2019-03-05 20:22:46 +0300

    update lazygit

--- Formula/lazygit.rb
+++ Formula/lazygit.rb
@@ -1,9 +1,9 @@
 class Lazygit < Formula
   desc "A simple terminal UI for git commands, written in Go"
   homepage "https://github.com/jesseduffield/lazygit/"
-  url "https://github.com/jesseduffield/lazygit/releases/download/v0.6/lazygit_0.6_Darwin_x86_64.tar.gz"
-  version "0.6"
-  sha256 "d1fd618cf2068e4ddb5a79ddb82fa4b94fafe9975c26a6c8c23da5bcd1707ca7"
+  url "https://github.com/jesseduffield/lazygit/releases/download/v0.7.2/lazygit_0.7.2_Darwin_x86_64.tar.gz"
+  version "0.7.2"
+  sha256 "a51fb5bebcde0c01b21ee8b2d75e13846601f9b996eb23ef87f5ea89775f0737"
 
   def install
     bin.install "lazygit"
