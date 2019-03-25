efdaf3fa3514febcdf477d14e2693c4b9b67e012
Les Aker <me@lesaker.org>
2019-03-24 21:40:37 -0400

    update qt

--- Formula/qt.rb
+++ Formula/qt.rb
@@ -3,8 +3,8 @@
 class Qt < Formula
   desc "Cross-platform application and UI framework"
   homepage "https://www.qt.io/"
-  url "https://download.qt.io/official_releases/qt/5.12/5.12.1/single/qt-everywhere-src-5.12.1.tar.xz"
-  sha256 "caffbd625c7bc10ff8c5c7a27dbc7d84fa4de146975c0e1ffe904b514ccd6da4"
+  url "https://download.qt.io/official_releases/qt/5.12/5.12.2/single/qt-everywhere-src-5.12.2.tar.xz"
+  sha256 "59b8cb4e728450b21224dcaaa40eb25bafc5196b6988f2225c394c6b7f881ff5"
 
   keg_only "Qt 5 has CMake issues when linked"
 
