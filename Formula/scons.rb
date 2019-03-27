dc35ed607a1185ecbea2c3ebef7435abd360d154
Les Aker <me@lesaker.org>
2019-03-27 22:39:36 +0000

    update scons

--- Formula/scons.rb
+++ Formula/scons.rb
@@ -1,8 +1,8 @@
 class Scons < Formula
   desc "Substitute for classic 'make' tool with autoconf/automake functionality"
   homepage "https://www.scons.org/"
-  url "https://downloads.sourceforge.net/project/scons/scons/3.0.4/scons-3.0.4.tar.gz"
-  sha256 "e2b8b36e25492720a05c0f0a92a219b42d11ce0c51e3397a1e8296dfea1d9b1a"
+  url "https://downloads.sourceforge.net/project/scons/scons/3.0.5/scons-3.0.5.tar.gz"
+  sha256 "df676f23dc6d4bfa384fc389d95dcd21ab907e6349d4c848958ba4befb73c73e"
 
   def install
     man1.install gzip("scons-time.1", "scons.1", "sconsign.1")
