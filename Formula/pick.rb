b6653957dc98780b2d92b1651072dcc6d1e6fbe8
Les Aker <me@lesaker.org>
2019-04-01 21:03:52 +0100

    update pick formula

--- Formula/pick.rb
+++ Formula/pick.rb
@@ -1,12 +1,13 @@
 class Pick < Formula
   desc "Command line arg picker"
-  homepage "https://github.com/thoughtbot/pick"
-  url "https://github.com/thoughtbot/pick/releases/download/v3.0.1/pick-3.0.1.tar.gz"
+  homepage "https://github.com/mptre/pick"
+  url "https://github.com/mptre/pick/releases/download/v3.0.1/pick-3.0.1.tar.gz"
   sha256 "668c863751f94ad90e295cf861a80b4d94975e06645f401d7f82525e607c0266"
 
 
   def install
-    system "./configure", "--prefix=#{prefix}"
+    ENV["PREFIX"] = prefix
+    system "./configure"
     system "make", "install"
   end
 
