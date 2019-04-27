83d8106513c60f2e042ef8746e5b465f55e6a9b8
Les Aker <me@lesaker.org>
2019-04-27 13:37:11 +0100

    update icu4c

--- Formula/icu4c.rb
+++ Formula/icu4c.rb
@@ -1,15 +1,21 @@
 class Icu4c < Formula
   desc "C/C++ and Java libraries for Unicode and globalization"
-  homepage "http://site.icu-project.org/"
-  url "https://ssl.icu-project.org/files/icu4c/63.1/icu4c-63_1-src.tgz"
-  version "63.1"
-  sha256 "05c490b69454fce5860b7e8e2821231674af0a11d7ef2febea9a32512998cb9d"
+  homepage "https://ssl.icu-project.org/"
+  url "https://ssl.icu-project.org/files/icu4c/64.2/icu4c-64_2-src.tgz"
+  mirror "https://github.com/unicode-org/icu/releases/download/release-64-2/icu4c-64_2-src.tgz"
+  version "64.2"
+  sha256 "627d5d8478e6d96fc8c90fed4851239079a561a6a8b9e48b0892f24e82d31d6c"
 
   keg_only :provided_by_macos, "macOS provides libicucore.dylib (but nothing else)"
 
   def install
-    args = %W[--prefix=#{prefix} --disable-samples --disable-tests --enable-static]
-    args << "--with-library-bits=64" if MacOS.prefer_64_bit?
+    args = %W[
+      --prefix=#{prefix}
+      --disable-samples
+      --disable-tests
+      --enable-static
+      --with-library-bits=64
+    ]
 
     cd "source" do
       system "./configure", *args
